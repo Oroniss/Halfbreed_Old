5db6ab667e8cbb64ab807ab61af978fdd03f290a
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-03 16:52:18 +1100

    * MainProgram.cs: Began the main program - still needs a lot of   work, but at least it runs.
    * packages.config:
    * OpenTK.dll.config: Add required package.
    
    * terminal8x8.png: Add font file - temporary until a nicer one can be
      found.

---
+++ Halfbreed/Halfbreed/MainProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RLNET;
+
+namespace Halfbreed
+{
+    public static class MainProgram
+    {
+        private static RLRootConsole mainconsole;
+
+        public static void Main()
+        {
+            MainProgram.mainconsole = new RLRootConsole("terminal8x8.png", 80, 60, 8, 8, 1, "Halfbreed");
+
+            mainconsole.OnLoad += RootConsoleOnLoad;
+            mainconsole.Update += RootConsoleUpdate;
+            mainconsole.Render += RootConsoleRender;
+            mainconsole.Run();
+        }
+
+        static void RootConsoleOnLoad(object sender, EventArgs e)
+        {
+            mainconsole.SetWindowState(RLWindowState.Fullscreen);
+        }
+
+        static void RootConsoleRender(object sender, EventArgs e)
+        {
+            mainconsole.Clear();
+            mainconsole.Print(5, 5, "Hello World", RLColor.Cyan);
+            mainconsole.Draw();
+        }
+        static void RootConsoleUpdate(object sender, EventArgs e)
+        {
+            RLKeyPress key = mainconsole.Keyboard.GetKeyPress();
+            if (key != null)
+            {
+                switch (key.Key)
+                {
+                    case RLKey.Escape:
+                    {
+                        mainconsole.Close();
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
