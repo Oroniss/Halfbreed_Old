4be5fcca2e110cf91b6f97331b65a13fba7ab0b6
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-06 11:54:33 +1100

    Setup first unit tests for key converter.

--- Halfbreed/Halfbreed/Tests/InputTests.cs
+++ Halfbreed/Halfbreed/Tests/InputTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-namespace Halfbreed.Tests
+using RLNET;
+using NUnit.Framework;
+
+namespace Halfbreed
 {
+    [TestFixture]
     public class InputTests
     {
-        public InputTests()
+        private KeyToStringConverter converter = new KeyToStringConverter();
+
+        [Test]
+        public void TestCheckKeyIsValid()
         {
+            Assert.IsFalse(converter.checkKeyIsValid(RLKey.Tab));
+            Assert.IsTrue(converter.checkKeyIsValid(RLKey.Escape));
+        }
+
+        [Test]
+        public void TestConvertKeyToString()
+        {
+            Assert.IsTrue(converter.convertKeyToString(RLKey.Escape) == "ESCAPE");
+            Assert.IsTrue(converter.convertKeyToString(RLKey.Left) == "LEFT");
+            Assert.IsTrue(converter.convertKeyToString(RLKey.Right) == "RIGHT");
         }
     }
 }
