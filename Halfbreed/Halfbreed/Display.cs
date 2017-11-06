1b1c59e45292afd198dca583536741ea935c09b3
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-06 14:18:08 +1100

    Added a backconsole - need to figure out how to lock it for thread safe updates and fetches.

---
+++ Halfbreed/Halfbreed/Display.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using RLNET;
+
+namespace Halfbreed
+{
+    public static class Display
+    {
+		// TODO: Refactor out the magic numbers here
+		private static RLConsole _backConsole = new RLConsole(120, 90);
+		private static bool _isDirty = true;
+
+		public static bool IsDirty
+		{
+			get { return _isDirty; }
+		}
+    }
+}
