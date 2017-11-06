9ed90df4fd5a5de5cf49eca0e02a0fe9501fab46
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-06 17:59:53 +1100

    Added a copy to main console method

--- Halfbreed/Halfbreed/Display.cs
+++ Halfbreed/Halfbreed/Display.cs
@@ -7,11 +7,30 @@ namespace Halfbreed
     {
 		// TODO: Refactor out the magic numbers here
 		private static RLConsole _backConsole = new RLConsole(120, 90);
+		private static RLConsole _menuConsole = new RLConsole(116, 86);
+
 		private static bool _isDirty = true;
+
+		static Display()
+		{
+			_menuConsole.SetBackColor(0, 0, 116, 86, RLColor.LightGray);
+			_backConsole.Print(5, 5, "Hello", RLColor.Cyan);
+		}
 
 		public static bool IsDirty
 		{
 			get { return _isDirty; }
+		}
+
+		public static RLRootConsole CopyDisplayToMainConsole(RLRootConsole destination)
+		{
+			lock (_backConsole)
+			{
+				RLConsole.Blit(_backConsole, 0, 0, 120, 90, destination, 0, 0);
+			}
+			// TODO: Decide if this needs to be inside a lock too.
+			_isDirty = false;
+			return destination;
 		}
     }
 }
