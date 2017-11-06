df02e7683914f7e01dcd163598141d6264a49abd
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-06 11:53:39 +1100

    Started the input handler class. Responsible for dealing with any keyboard strokes. May get re-named in the future if mouse handling becomes a thing.

--- Halfbreed/Halfbreed/UserInputHandler.cs
+++ Halfbreed/Halfbreed/UserInputHandler.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using RLNET;
+
 namespace Halfbreed
 {
-    public class UserInputHandler
+    public static class UserInputHandler
     {
-        public UserInputHandler()
+        private static List<String> _queuedInput = new List<String>();
+        private static KeyToStringConverter _keyConverter = new KeyToStringConverter();
+
+        public static void addKeyboardInput(RLKey key)
         {
+            if(_keyConverter.checkKeyIsValid(key))
+            {
+                lock (_queuedInput)
+                {
+                    _queuedInput.Add(_keyConverter.convertKeyToString(key));
+                }
+            }
+        }
+
+        public static String getNextKey()
+        {
+            while (true)
+            {
+                lock (_queuedInput)
+                {
+                    if(_queuedInput.Count > 0)
+                    {
+                        String toReturn = _queuedInput[0];
+                        _queuedInput = new List<string>();
+                        return toReturn;
+                    }
+                }
+                Thread.Yield();
+            }
         }
     }
 }
