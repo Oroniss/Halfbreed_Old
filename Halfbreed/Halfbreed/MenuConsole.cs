557a747403a9efb7b06042948baeac3d43f42137
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-07 10:22:54 +1100

    Made this a subclass - added a DrawSelectFromMenu method.

--- Halfbreed/Halfbreed/MenuConsole.cs
+++ Halfbreed/Halfbreed/MenuConsole.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using RLNET;
 namespace Halfbreed
 {
-	public class MenuConsole
+	public class MenuConsole : DisplayConsole
 	{
-		public MenuConsole()
+		public MenuConsole(int height, int width, int top, int left, RLColor backColor, BackConsole backConsole)
+			: base(height, width, top, left, backColor, backConsole)
 		{
+		}
+
+		public void DrawSelectFromMenu(string title, string[] options, string bottom)
+		{
+			_console.Print(5, 5, title, RLColor.Black);
+
+			for (int i = 0; i < options.Length; i++)
+			{
+				_console.Print(5, 10 + 4 * i, options[i], RLColor.Black);
+			}
+
+			_console.Print(5, 70, bottom, RLColor.Black);
+
+			CopyToBackConsole();
 		}
 	}
 }
