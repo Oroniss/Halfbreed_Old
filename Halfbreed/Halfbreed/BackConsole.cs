879642e960578973dd1bdfc5e4736e2c29fd8370
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-07 00:24:59 +1100

    Created a back console - inherits from RLConsole, allows for a set dirty to provide thread locking.

---
+++ Halfbreed/Halfbreed/BackConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RLNET;
+namespace Halfbreed
+{
+	public class BackConsole:RLConsole
+	{
+		bool _isdirty;
+
+		public BackConsole(int width, int height)
+			:base(width, height)
+		{
+			_isdirty = true;
+		}
+
+		public bool IsDirty
+		{
+			get { return _isdirty; }
+		}
+
+		public void SetClean()
+		{
+			_isdirty = false;
+		}
+
+		public void SetDirty()
+		{
+			_isdirty = true;
+		}
+	}
+}
