fecc85c32d5815c92f76f099e37a871c85b7191c
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 00:23:17 +1100

    Added a Clear function to reset the console, including the background color.

--- Halfbreed/Halfbreed/Display/DisplayConsole.cs
+++ Halfbreed/Halfbreed/Display/DisplayConsole.cs
@@ -7,6 +7,7 @@ namespace Halfbreed
 		int _width;
 		int _top;
 		int _left;
+		RLColor _backColor;
 		BackConsole _backConsole;
 		protected RLConsole _console;
 
@@ -17,6 +18,7 @@ namespace Halfbreed
 			_left = left;
 			_top = top;
 			_backConsole = backConsole;
+			_backColor = backColor;
 
 			_console = new RLConsole(_width, _height);
 			_console.SetBackColor(0, 0, _width, _height, backColor);
@@ -29,6 +31,12 @@ namespace Halfbreed
 				RLConsole.Blit(_console, 0, 0, _width, _height, _backConsole, _left, _top);
 				_backConsole.SetDirty();
 			}
+		}
+
+		protected void Clear()
+		{
+			_console.Clear();
+			_console.SetBackColor(0, 0, _width, _height, _backColor);
 		}
 	}
 }
