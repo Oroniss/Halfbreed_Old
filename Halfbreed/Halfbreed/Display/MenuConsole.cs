c42f801638bb08786fb97d28f8c6c5d4f89d88a8
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 00:23:41 +1100

    Minor bug fix with clear.

--- Halfbreed/Halfbreed/Display/MenuConsole.cs
+++ Halfbreed/Halfbreed/Display/MenuConsole.cs
@@ -12,8 +12,11 @@ namespace Halfbreed
 
 		public void DrawMenu(string title, List<string> options, string bottom)
 		{
+			Clear();
+
 			_console.Print(5, 5, title, RLColor.Black);
 
+			// TODO: Figure out whether we can dynamically space the menu between 1 and 4 spaces.
 			for (int i = 0; i < options.Count; i++)
 			{
 				_console.Print(5, 10 + 4 * i, options[i], RLColor.Black);
