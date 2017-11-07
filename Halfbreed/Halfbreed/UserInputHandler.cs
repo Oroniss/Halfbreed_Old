ccb792d9ebe8836174b9a1185c6a5bd2867bb139
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-07 22:39:34 +1100

    Added a select from menu function - should be general enough to use in most places.

--- Halfbreed/Halfbreed/UserInputHandler.cs
+++ Halfbreed/Halfbreed/UserInputHandler.cs
@@ -10,6 +10,9 @@ namespace Halfbreed
         private static List<String> _queuedInput = new List<String>();
         private static KeyToStringConverter _keyConverter = new KeyToStringConverter();
 
+		private static List<String> _numberKeys = new List<String>
+		    {"1", "2", "3", "4", "5", "6", "7", "8", "9",  "0"};
+
         public static void addKeyboardInput(RLKey key)
         {
             if(_keyConverter.checkKeyIsValid(key))
@@ -37,5 +40,42 @@ namespace Halfbreed
                 Thread.Yield();
             }
         }
+
+		public static int SelectFromMenu(string title, List<String> menuOptions, string bottom)
+		{
+			int page = 0;
+			List<String> currentDisplay = new List<string>();
+			while (true)
+			{
+				for (int i = 10 * page; i < Math.Min(10 * (page + 1), menuOptions.Count); i++)
+				{
+					currentDisplay.Add((i % 10 + 1).ToString() + ": " + menuOptions[i]);
+				}
+				GraphicDesplay.MenuConsole.DrawMenu(title, currentDisplay, bottom);
+
+				string key = getNextKey();
+
+				if (_numberKeys.Contains(key))
+				{
+					int num = Int32.Parse(key);
+					if (num + page * 10 <= menuOptions.Count)
+					{
+						return num - 1 + page * 10;
+					}
+				}
+				if (key == "LEFT" && page > 0)
+				{
+					page--;
+				}
+				if (key == "RIGHT" && ((page + 1) * 10 < menuOptions.Count))
+				{
+					page++;
+				}
+				if (key == "ESCAPE")
+				{
+					return -1;
+				}
+			}
+		}
     }
 }
