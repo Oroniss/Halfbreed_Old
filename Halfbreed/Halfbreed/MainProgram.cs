c37a533697a7ba82cdccf9242be65ec5fb15be60
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-07 13:53:42 +1100

    Cleared some stuff that will go elsewhere. Also started up the game loop thread.

--- Halfbreed/Halfbreed/MainProgram.cs
+++ Halfbreed/Halfbreed/MainProgram.cs
@@ -10,16 +10,14 @@ namespace Halfbreed
 
         public static void Main()
         {
-            getConfigurationParameters();
-
             MainProgram.mainconsole = new RLRootConsole("terminal8x8.png", 120, 90, 8, 8, 1, "Halfbreed");
 
             mainconsole.OnLoad += RootConsoleOnLoad;
             mainconsole.Update += RootConsoleUpdate;
             mainconsole.Render += RootConsoleRender;
 
-			// Thread loopThread = new Thread(gameInterface.StartGameMenus);
-			// loopThread.start()
+			Thread loopThread = new Thread(StartGameMenus.TitleMenu);
+			loopThread.Start();
 
             mainconsole.Run();
         }
@@ -47,30 +45,17 @@ namespace Halfbreed
                 UserInputHandler.addKeyboardInput(key.Key);
 
                 // TODO: Remove this once threading properly in.
-                if(key.Key == RLKey.Escape)
+                if(key.Key == RLKey.Up)
                 {
                     mainconsole.Close();
                 }
-				if (key.Key == RLKey.Enter)
-				{
-					GraphicDesplay.MenuConsole.DrawSelectFromMenu("Boo", new string[0], "Goodbye");
-				}
             }
         }
 
-        static void checkSetup()
-        {
-            if (!System.IO.Directory.Exists("Foo"))
-            {
-
-            }
-
-        }
-
-        static void getConfigurationParameters()
-        {
-            
-        }
+		public static void quit()
+		{
+			mainconsole.Close();
+		}
 
     }
 }
