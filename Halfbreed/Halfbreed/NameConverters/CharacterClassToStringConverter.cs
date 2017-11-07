7a05a0bb9afe9284f0364db411aa59f6a07d35db
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 09:50:54 +1100

    Redid to fix breakage when file was moved.

--- Halfbreed/Halfbreed/NameConverters/CharacterClassToStringConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/CharacterClassToStringConverter.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using System.Collections.Generic;
 namespace Halfbreed
 {
 	public class CharacterClassToStringConverter
 	{
-		public CharacterClassToStringConverter()
+		// TODO: Add upper case versions.
+		private static Dictionary<string, CharacterClasses> _stringToCharacterClass =
+			new Dictionary<string, CharacterClasses>
 		{
+			{"Cleric", CharacterClasses.CLERIC},
+			{"Fighter", CharacterClasses.FIGHTER},
+			{"Mage", CharacterClasses.MAGE},
+			{"Thief", CharacterClasses.THIEF},
+			{"Bard", CharacterClasses.BARD},
+			{"Blackguard", CharacterClasses.BLACKGUARD},
+			{"Druid", CharacterClasses.DRUID},
+			{"Necromancer", CharacterClasses.NECROMANCER},
+			{"Paladin", CharacterClasses.PALADIN},
+			{"Ranger", CharacterClasses.RANGER},
+			{"Dragonlord", CharacterClasses.DRAGONLORD}
+		};
+
+		public static CharacterClasses ConvertStringToCharacterClass(string characterClassName)
+		{
+			return _stringToCharacterClass[characterClassName];
 		}
+
 	}
 }
