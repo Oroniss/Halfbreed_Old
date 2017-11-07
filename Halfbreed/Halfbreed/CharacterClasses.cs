c4ad231371f517c8f8ee5b0c984cee63460396d7
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 00:22:43 +1100

    Created the character class enum.

---
+++ Halfbreed/Halfbreed/CharacterClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Halfbreed
+{
+	public enum CharacterClasses
+	{
+		CLERIC,
+		FIGHTER,
+		MAGE,
+		THIEF,
+		BARD,
+		BLACKGUARD,
+		DRUID,
+		NECROMANCER,
+		PALADIN,
+		RANGER,
+		DRAGONLORD
+	}
+}
