915c04217f80a566eaa0dd91771e358a0bf199fa
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-07 22:37:43 +1100

    Stripped the select from menu function out - just made a draw menu function so input can be elsewhere.

--- Halfbreed/Halfbreed/Display/MenuConsole.cs
+++ Halfbreed/Halfbreed/Display/MenuConsole.cs
@@ -9,17 +9,17 @@ namespace Halfbreed
 		{
 		}
 
-		// TODO: Think about whether to have an override with an array here?
-		public void DrawSelectFromMenu(string title, List<string> options, string bottom)
+
+		public void DrawMenu(string title, List<string> options, string bottom)
 		{
 			_console.Print(5, 5, title, RLColor.Black);
 
 			for (int i = 0; i < options.Count; i++)
 			{
-				_console.Print(5, 10 + 4 * i, (i+1).ToString() + ": " + options[i], RLColor.Black);
+				_console.Print(5, 10 + 4 * i, options[i], RLColor.Black);
 			}
 
-			_console.Print(5, 70, bottom, RLColor.Black);
+			_console.Print(5, 80, bottom, RLColor.Black);
 
 			CopyToBackConsole();
 		}
