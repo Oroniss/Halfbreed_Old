8702ca67477c48c00c3c144741ac3005880b4109
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 17:10:46 +1100

    Renamed.

--- Halfbreed/Halfbreed/Tests/UserInputTests.cs
+++ Halfbreed/Halfbreed/Tests/UserInputTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace Halfbreed
 {
     [TestFixture]
-    public class InputTests
+    public class UserInputTests
     {
 		[Test]
 		public void TestKeyBoardInputSimulator()
