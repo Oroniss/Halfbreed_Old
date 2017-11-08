bc352b39b2c48ed54b825dd42da19fcc92113685
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 10:50:19 +1100

    Starting level class - will likely morph significantly.

--- Halfbreed/Halfbreed/LevelCode/Level.cs
+++ Halfbreed/Halfbreed/LevelCode/Level.cs
@@ -1,13 +1,50 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System;
 namespace Halfbreed
 {
-	public class Zone
+	public class Level
 	{
-		public Zone(string zoneID)
+		private string _levelName;
+		private int _levelHeight;
+		private int _levelWidth;
+		private int _lightingLevel;
+		private float _anathemaMultiplier;
+
+		public Level(string levelID)
 		{
-			string[] zoneSpecification = File.ReadAllLines("Levels/" + zoneID + ".txt");
+			FileStream levelFile = File.Open(levelID + ".txt", FileMode.Open);
+
+			StreamReader reader = new StreamReader(levelFile);
+
+			_levelName = reader.ReadLine();
+			_levelHeight = Int32.Parse(reader.ReadLine());
+			_levelWidth = Int32.Parse(reader.ReadLine());
+			_lightingLevel = Int32.Parse(reader.ReadLine());
+			_anathemaMultiplier = float.Parse(reader.ReadLine());
+
+			levelFile.Close();
 		}
 
+		public string LevelName
+		{
+			get { return _levelName; }
+		}
+		public int Height
+		{
+			get { return _levelHeight; }
+		}
+		public int Width
+		{
+			get { return _levelWidth; }
+		}
+		public int LightingLevel
+		{
+			get { return _lightingLevel; }
+		}
+		public float AnathemaModifier
+		{
+			get { return _anathemaMultiplier; }
+		}
 	}
 }
