72e8fb9d0e7a896e4b524172af3cc0ae095ff12f
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 13:31:48 +1100

    Added tests for keyboard input simulator, get next key and select from menu. Generally refactored tests to use AreEquals instead of IsTrue (a == b)

--- Halfbreed/Halfbreed/Tests/InputTests.cs
+++ Halfbreed/Halfbreed/Tests/InputTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using RLNET;
 using NUnit.Framework;
 
@@ -7,21 +7,97 @@ namespace Halfbreed
     [TestFixture]
     public class InputTests
     {
-        private KeyToStringConverter converter = new KeyToStringConverter();
-
         [Test]
         public void TestCheckKeyIsValid()
         {
-            Assert.IsFalse(converter.checkKeyIsValid(RLKey.Tab));
-            Assert.IsTrue(converter.checkKeyIsValid(RLKey.Escape));
+            Assert.IsFalse(KeyToStringConverter.checkKeyIsValid(RLKey.Tab));
+            Assert.IsTrue(KeyToStringConverter.checkKeyIsValid(RLKey.Escape));
         }
 
         [Test]
         public void TestConvertKeyToString()
         {
-            Assert.IsTrue(converter.convertKeyToString(RLKey.Escape) == "ESCAPE");
-            Assert.IsTrue(converter.convertKeyToString(RLKey.Left) == "LEFT");
-            Assert.IsTrue(converter.convertKeyToString(RLKey.Right) == "RIGHT");
+            Assert.AreEqual("ESCAPE", KeyToStringConverter.convertKeyToString(RLKey.Escape));
+            Assert.AreEqual("LEFT", KeyToStringConverter.convertKeyToString(RLKey.Left));
+            Assert.AreEqual("RIGHT", KeyToStringConverter.convertKeyToString(RLKey.Right));
         }
+
+		[Test]
+		public void TestKeyBoardInputSimulator()
+		{
+			KeyBoardInputSimulator.AddKeyBoardInput("LEFT");
+			Assert.AreEqual(UserInputHandler.getNextKey(), "LEFT");
+
+			KeyBoardInputSimulator.AddKeyBoardInput("ESCAPE");
+			Assert.AreEqual("ESCAPE", UserInputHandler.getNextKey());
+
+			string[] keyArray = new string[] { "LEFT", "1", "7", "UP", "DOWN" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+
+			Assert.AreEqual("LEFT", UserInputHandler.getNextKey());
+			Assert.AreEqual("1", UserInputHandler.getNextKey());
+			Assert.AreEqual("7", UserInputHandler.getNextKey());
+			Assert.AreEqual("UP", UserInputHandler.getNextKey());
+			Assert.AreEqual("DOWN", UserInputHandler.getNextKey());
+
+		}
+
+		[Test]
+		public void TestSelectFromMenu()
+		{
+			List<string> menuOptions = new List<string>();
+			for (int i = 0; i < 35; i++)
+			{
+				menuOptions.Add(i.ToString());
+			}
+
+			string title = "";
+			string end = "";
+
+			string[] keyArray = new string[] { "3" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(2, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "UP", "7" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(6, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "LEFT", "5" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(4, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "0" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(9, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "UP", "UP", "7" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(6, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "7" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(16, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "4" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(33, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "RIGHT", "0" };
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(29, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "RIGHT", "LEFT", "5"};
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(14, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "8", "LEFT", "6"};
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(25, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "8", "ESCAPE"};
+			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
+			Assert.AreEqual(-1, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+		}
     }
 }
