499cadf58526928fe12aff3370d4ec1f58c7eeed
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 16:34:51 +1100

    Created a file to test any class that goes in the Converters Folder Added tests for Character Class To String converter Put the Key to String tests into here.

--- Halfbreed/Halfbreed/Tests/NameConverterTests.cs
+++ Halfbreed/Halfbreed/Tests/NameConverterTests.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using NUnit.Framework;
+using RLNET;
+
 namespace Halfbreed
 {
+	[TestFixture]
 	public class NameConverterTests
 	{
-		public NameConverterTests()
+
+		[Test]
+		public void TestKeyToStringConverter()
 		{
+			Assert.IsFalse(KeyToStringConverter.checkKeyIsValid(RLKey.Tab));
+			Assert.IsTrue(KeyToStringConverter.checkKeyIsValid(RLKey.Escape));
+
+			Assert.AreEqual("ESCAPE", KeyToStringConverter.convertKeyToString(RLKey.Escape));
+			Assert.AreEqual("LEFT", KeyToStringConverter.convertKeyToString(RLKey.Left));
+			Assert.AreEqual("RIGHT", KeyToStringConverter.convertKeyToString(RLKey.Right));
 		}
+
+		[Test]
+		public void TestCharacterClassToStringConverter()
+		{
+			Assert.AreEqual(CharacterClasses.BARD,
+							CharacterClassToStringConverter.ConvertStringToCharacterClass("Bard"));
+			Assert.AreEqual(CharacterClasses.PALADIN,
+							CharacterClassToStringConverter.ConvertStringToCharacterClass("Paladin"));
+			Assert.AreEqual(CharacterClasses.FIGHTER,
+							CharacterClassToStringConverter.ConvertStringToCharacterClass("Fighter"));
+			Assert.AreEqual(CharacterClasses.DRAGONLORD,
+							CharacterClassToStringConverter.ConvertStringToCharacterClass("Dragonlord"));
+		}
+
 	}
 }
