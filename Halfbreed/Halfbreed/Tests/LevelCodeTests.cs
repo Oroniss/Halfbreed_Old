3d8c4ab71ed2d2a80cce02b8a969846b5d6d5504
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 10:51:21 +1100

    Added a basic test for level reading - still needs work.

--- Halfbreed/Halfbreed/Tests/LevelCodeTests.cs
+++ Halfbreed/Halfbreed/Tests/LevelCodeTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using NUnit.Framework;
+
 namespace Halfbreed
 {
+	[TestFixture]
 	public class LevelCodeTests
 	{
-		public LevelCodeTests()
+		[Test]
+		public void TestLevelConstruction()
 		{
-		}
+			string filePath = TestContext.CurrentContext.TestDirectory + "/Tests/TestLevel";
+			Level testLevel = new Level(filePath);
+
+			Assert.AreEqual(25, testLevel.Height);
+			Assert.AreEqual(44, testLevel.Width);
+			Assert.AreEqual("Wall Market Square Storage Facility - Temporary Storage", testLevel.LevelName);
+			Assert.AreEqual(-6, testLevel.LightingLevel);
+			Assert.IsTrue(Math.Abs( 0.5 -  testLevel.AnathemaModifier) < 0.00001);
+
+		}	
+
 	}
 }
