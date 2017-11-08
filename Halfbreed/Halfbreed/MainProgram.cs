67d145d521236df1a6c4bd457f5e6fb23a124569
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-08 13:32:38 +1100

    Put the key conversion inside "update" instead of leaving it to input handler.

--- Halfbreed/Halfbreed/MainProgram.cs
+++ Halfbreed/Halfbreed/MainProgram.cs
@@ -42,7 +42,8 @@ namespace Halfbreed
             RLKeyPress key = mainconsole.Keyboard.GetKeyPress();
             if (key != null)
             {
-                UserInputHandler.addKeyboardInput(key.Key);
+				if (KeyToStringConverter.checkKeyIsValid(key.Key))
+					UserInputHandler.addKeyboardInput(KeyToStringConverter.convertKeyToString(key.Key));
             }
         }
 
