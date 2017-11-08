0b3c7f975f965dec38deaa6179634c4fffba90f7
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 10:49:45 +1100

    Added for testing purposes

---
+++ Halfbreed/Halfbreed/LevelCode/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace Halfbreed
+{
+	public class Zone
+	{
+		public Zone(string zoneID)
+		{
+			string[] zoneSpecification = File.ReadAllLines("Levels/" + zoneID + ".txt");
+		}
+
+	}
+}
