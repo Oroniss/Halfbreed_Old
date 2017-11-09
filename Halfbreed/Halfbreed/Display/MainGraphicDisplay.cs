0f6f1f8741e497212f7f9b5e684c71559041f229
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-10 10:21:58 +1100

    Added the map console.

--- Halfbreed/Halfbreed/Display/MainGraphicDisplay.cs
+++ Halfbreed/Halfbreed/Display/MainGraphicDisplay.cs
@@ -7,6 +7,7 @@ namespace Halfbreed
 		// TODO: Refactor out the magic numbers here
 		private static BackConsole _backConsole = new BackConsole(120, 90);
 		private static MenuConsole _menuConsole = new MenuConsole(86, 116, 2, 2, RLColor.LightGray, _backConsole);
+		private static MapConsole _mapConsole = new MapConsole(60, 60, 0, 0, Palette.BLACK, _backConsole);
 
 		static MainGraphicDisplay()
 		{
@@ -37,6 +38,11 @@ namespace Halfbreed
 		public static MenuConsole MenuConsole
 		{
 			get { return _menuConsole; }
+		}
+
+		public static MapConsole MapConsole
+		{
+			get { return _mapConsole; }
 		}
 
     }
