b1f1ff8832006224764aab905a41e9745c2770c7
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 15:52:26 +1100

    Discarded for now.

---
+++ Halfbreed/Halfbreed/NameConverters/StringToColorConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Halfbreed
+{
+	public class StringToColorConverter
+	{
+		public StringToColorConverter()
+		{
+		}
+	}
+}
---
+++ Halfbreed/Halfbreed/Palette.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Halfbreed
+{
+	public class Palette
+	{
+		public Palette()
+		{
+		}
+	}
+}
