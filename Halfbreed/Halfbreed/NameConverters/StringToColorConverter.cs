17578022f4c0aac9da357654e3b643815b1a580b
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 16:06:31 +1100

    Added this class to converters.

--- Halfbreed/Halfbreed/NameConverters/StringToColorConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/StringToColorConverter.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using RLNET;
+
 namespace Halfbreed
 {
-	public class StringToColorConverter
+	public static class StringToColorConverter
 	{
-		public StringToColorConverter()
+		private static Dictionary<string, RLColor> stringToColorDict = new Dictionary<string, RLColor>()
 		{
+			// TODO: Keep this up to date.
+			{"White", Palette.WHITE},
+			{"Black", Palette.BLACK},
+			{"Wood Brown", Palette.WOODBROWN},
+			{"Dark Wood Brown", Palette.DARKWOODBROWN}
+		};
+
+		public static RLColor ConvertStringToColor(string colorName)
+		{
+			return stringToColorDict[colorName];
 		}
+
 	}
 }
