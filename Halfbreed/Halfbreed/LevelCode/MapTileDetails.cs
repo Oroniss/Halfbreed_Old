b0805e4e4d413cef07f9c1664eb6a17bd4fd0fd1
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 16:42:06 +1100

    Setup a class/struct for map tiles. Might want to read it in from a file eventually.

---
+++ Halfbreed/Halfbreed/LevelCode/MapTileDetails.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using RLNET;
+
+namespace Halfbreed
+{
+	public static class MapTileDetails
+	{
+		public static Dictionary<TileType, TileDetails> MapTileDict = new Dictionary<TileType, TileDetails>()
+		{
+			// TODO: Eventually this should probably be read in from somewhere.
+			// TODO: At least think about it.
+			{TileType.WOODFLOOR, new TileDetails("Wood Floor", 0, 0, true, "Wood Brown", "Wood Brown")},
+			{TileType.WOODWALL, new TileDetails("Wood Wall", 20, 4, false, "Dark Wood Brown", "Dark Wood Brown")},
+			{TileType.WOODENDEBRIS, new TileDetails("Wooden Debris", 20, 4, false, "Dark Wood Brown", "Dark Wood Brown")}
+		};
+
+	}
+
+	public struct TileDetails
+	{
+		public string Name;
+		public int Elevation;
+		public int MoveTypes;
+		public bool AllowLOS;
+		public RLColor BGColor;
+		public RLColor FogColor;
+
+		internal TileDetails(string name, int elevation, int moveTypes, bool allowLOS, string bgColorName,
+							string fogColorName)
+		{
+			Name = name;
+			Elevation = elevation;
+			MoveTypes = moveTypes;
+			AllowLOS = allowLOS;
+			BGColor = StringToColorConverter.ConvertStringToColor(bgColorName);
+			FogColor = StringToColorConverter.ConvertStringToColor(fogColorName);
+		}
+	}
+
+}
+
