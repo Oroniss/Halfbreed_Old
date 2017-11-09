72ab0982d67bf95fed2b652e6d0b00cbcd45ab15
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 16:08:13 +1100

    Added a class to store all the custom colors. May look at recompiling RLNET in order to redefine the RLColor class, since I'd like to extend it with an HBColor to remove dependency and make it library indepedent.

--- Halfbreed/Halfbreed/Palette.cs
+++ Halfbreed/Halfbreed/Palette.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using RLNET;
+
 namespace Halfbreed
 {
-	public class Palette
+	public static class Palette
 	{
-		public Palette()
-		{
-		}
+		// TODO: Update with earlier values.
+		// TODO: Remove references to built in RLColors in consoles, etc.
+		public static readonly RLColor BLACK = new RLColor(0, 0, 0);
+		public static readonly RLColor WHITE = new RLColor(255, 255, 255);
+		public static readonly RLColor WOODBROWN = new RLColor(173, 119, 74);
+		public static readonly RLColor DARKWOODBROWN = new RLColor(122, 84, 52);
+
+
 	}
 }
