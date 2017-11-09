1ba49f747e0cb02b80a8d383217fe6087b075e55
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 12:03:59 +1100

    Built this out to only include map related stuff.

--- Halfbreed/Halfbreed/LevelCode/LevelMap.cs
+++ Halfbreed/Halfbreed/LevelCode/LevelMap.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
 namespace Halfbreed
 {
 	public class LevelMap
 	{
-		public LevelMap()
+		private int _height;
+		private int _width;
+		private TileType[] _mapGrid;
+
+		public LevelMap(string mapFilePath)
 		{
+			FileStream mapFile = File.Open(mapFilePath, FileMode.Open);
+			StreamReader MapSpecificationFile = new StreamReader(mapFile);
+
+			Dictionary<int, TileType > tileDict = new Dictionary<int, TileType>();
+
+			_height = Int32.Parse(MapSpecificationFile.ReadLine());
+			_width = Int32.Parse(MapSpecificationFile.ReadLine());
+
+			while (true)
+			{
+				string line = MapSpecificationFile.ReadLine().Trim();
+				if (line == "###")
+					break;
+
+				string[] splitLine = line.Split(',');
+				tileDict[Int32.Parse(splitLine[0])] = (TileType)Enum.Parse(typeof(TileType), splitLine[1]);
+			}
+
+			_mapGrid = new TileType[_width * _height];
+			for (int y = 0; y < _height; y++)
+			{
+				string[] _row = MapSpecificationFile.ReadLine().Trim().Split(',');
+				for (int x = 0; x < _width; x++)
+				{
+					_mapGrid[y * _width + x] = tileDict[Int32.Parse(_row[x])];
+				}
+			}
+
+			mapFile.Close();
 		}
 	}
 }
