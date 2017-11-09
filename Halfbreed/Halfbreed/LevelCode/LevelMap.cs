393348d520eb49fdac49798c13b0def750647950
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 13:21:15 +1100

    Built up the basic pieces of the map object. Still needs some work on revealed.

--- Halfbreed/Halfbreed/LevelCode/LevelMap.cs
+++ Halfbreed/Halfbreed/LevelCode/LevelMap.cs
@@ -8,9 +8,11 @@ namespace Halfbreed
 		private int _height;
 		private int _width;
 		private TileType[] _mapGrid;
+		private bool[] _revealed;
 
 		public LevelMap(string mapFilePath)
 		{
+			// TODO: Move this to a separate function to allow easy switching to resources.
 			FileStream mapFile = File.Open(mapFilePath, FileMode.Open);
 			StreamReader MapSpecificationFile = new StreamReader(mapFile);
 
@@ -18,6 +20,8 @@ namespace Halfbreed
 
 			_height = Int32.Parse(MapSpecificationFile.ReadLine());
 			_width = Int32.Parse(MapSpecificationFile.ReadLine());
+
+			MapSpecificationFile.ReadLine(); // Move to start of dictionary.
 
 			while (true)
 			{
@@ -30,16 +34,37 @@ namespace Halfbreed
 			}
 
 			_mapGrid = new TileType[_width * _height];
+			_revealed = new bool[_width * _height];
+
 			for (int y = 0; y < _height; y++)
 			{
 				string[] _row = MapSpecificationFile.ReadLine().Trim().Split(',');
 				for (int x = 0; x < _width; x++)
 				{
 					_mapGrid[y * _width + x] = tileDict[Int32.Parse(_row[x])];
+					_revealed[y * _width + x] = false;
 				}
 			}
 
 			mapFile.Close();
 		}
+
+		public int Height
+		{
+			get { return _height; }
+		}
+		public int Width
+		{
+			get { return _width; }
+		}
+		public bool IsValidMapCoord(int x, int y)
+		{
+			return x >= 0 && x < _width && y >= 0 && y < _height;
+		}
+		public TileType GetTile(int x, int y)
+		{
+			return _mapGrid[y * _width + x];
+		}
+
 	}
 }
