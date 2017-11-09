53ca50757fd1d169291aeba250920cf8fb97b1c8
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-09 13:22:24 +1100

    Added unit tests for the level map construction and functions.

--- Halfbreed/Halfbreed/Tests/LevelCodeTests.cs
+++ Halfbreed/Halfbreed/Tests/LevelCodeTests.cs
@@ -6,20 +6,52 @@ namespace Halfbreed
 	[TestFixture]
 	public class LevelCodeTests
 	{
+		private string testLevelMapFilePath = TestContext.CurrentContext.TestDirectory + "/LevelFiles/Testing/TestMap.txt";
+
 		[Test]
-		public void TestLevelConstruction()
+		public void TestLevelMapConstruction()
 		{
 			// TODO: Put a test in that tries to load every level to make sure it parses.
-			string filePath = TestContext.CurrentContext.TestDirectory + "/Tests/TestLevel";
-			Level testLevel = new Level(filePath);
+			LevelMap testMap = new LevelMap(testLevelMapFilePath);
 
-			Assert.AreEqual(25, testLevel.Height);
-			Assert.AreEqual(44, testLevel.Width);
-			Assert.AreEqual("Wall Market Square Storage Facility - Temporary Storage", testLevel.LevelName);
-			Assert.AreEqual(-6, testLevel.LightingLevel);
-			Assert.IsTrue(Math.Abs( 0.5 -  testLevel.AnathemaModifier) < 0.00001);
+			Assert.AreEqual(25, testMap.Height);
+			Assert.AreEqual(44, testMap.Width);
 
-		}	
+		}
 
+		[Test]
+		public void TestLevelMapIsValidMapCoords()
+		{
+			LevelMap testMap = new LevelMap(testLevelMapFilePath);
+
+			Assert.IsTrue(testMap.IsValidMapCoord(5, 5));
+			Assert.IsTrue(testMap.IsValidMapCoord(0, 0));
+			Assert.IsTrue(testMap.IsValidMapCoord(43, 24));
+			Assert.IsTrue(testMap.IsValidMapCoord(0, 24));
+			Assert.IsTrue(testMap.IsValidMapCoord(43, 0));
+			Assert.IsTrue(testMap.IsValidMapCoord(25, 15));
+
+			Assert.IsFalse(testMap.IsValidMapCoord(-1, 0));
+			Assert.IsFalse(testMap.IsValidMapCoord(0, -1));
+			Assert.IsFalse(testMap.IsValidMapCoord(24, 43));
+			Assert.IsFalse(testMap.IsValidMapCoord(50, 50));
+			Assert.IsFalse(testMap.IsValidMapCoord(44, 5));
+			Assert.IsFalse(testMap.IsValidMapCoord(-5, 25));
+
+
+		}
+
+		[Test]
+		public void TestLevelMapGetTile()
+		{
+			LevelMap testMap = new LevelMap(testLevelMapFilePath);
+
+			Assert.AreEqual(TileType.WOODWALL, testMap.GetTile(0, 0));
+			Assert.AreEqual(TileType.WOODWALL, testMap.GetTile(43, 24));
+			Assert.AreEqual(TileType.WOODFLOOR, testMap.GetTile(1, 1));
+			Assert.AreEqual(TileType.WOODFLOOR, testMap.GetTile(42, 1));
+			Assert.AreEqual(TileType.WOODENDEBRIS, testMap.GetTile(37, 8));
+			Assert.AreEqual(TileType.WOODENDEBRIS, testMap.GetTile(35, 10));
+		}
 	}
 }
