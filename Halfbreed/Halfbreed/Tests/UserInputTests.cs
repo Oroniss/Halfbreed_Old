795741fe44e703a5627a02d6f689e2c5703465cf
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-11 16:32:04 +1100

    Found a bug in the test suite where results of one test could impact on others. Also reworked for changes to character creation setup.

--- Halfbreed/Halfbreed/Tests/UserInputTests.cs
+++ Halfbreed/Halfbreed/Tests/UserInputTests.cs
@@ -9,11 +9,17 @@ namespace Halfbreed
 		[Test]
 		public void TestKeyBoardInputSimulator()
 		{
+			UserInputHandler.clearAllInput();
+
 			KeyBoardInputSimulator.AddKeyBoardInput("LEFT");
 			Assert.AreEqual(UserInputHandler.getNextKey(), "LEFT");
 
+			UserInputHandler.clearAllInput();
+
 			KeyBoardInputSimulator.AddKeyBoardInput("ESCAPE");
 			Assert.AreEqual("ESCAPE", UserInputHandler.getNextKey());
+
+			UserInputHandler.clearAllInput();
 
 			string[] keyArray = new string[] { "LEFT", "1", "7", "UP", "DOWN" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
@@ -24,6 +30,7 @@ namespace Halfbreed
 			Assert.AreEqual("UP", UserInputHandler.getNextKey());
 			Assert.AreEqual("DOWN", UserInputHandler.getNextKey());
 
+			UserInputHandler.clearAllInput();
 		}
 
 		[Test]
@@ -38,50 +45,73 @@ namespace Halfbreed
 			string title = "";
 			string end = "";
 
+			UserInputHandler.clearAllInput();
+
 			string[] keyArray = new string[] { "3" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(2, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			UserInputHandler.clearAllInput();
 
 			keyArray = new string[] { "UP", "7" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(6, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
+
 			keyArray = new string[] { "LEFT", "5" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(4, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			UserInputHandler.clearAllInput();
 
 			keyArray = new string[] { "0" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(9, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
+
 			keyArray = new string[] { "UP", "UP", "7" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(6, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			UserInputHandler.clearAllInput();
 
 			keyArray = new string[] { "RIGHT", "7" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(16, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
+
 			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "4" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(33, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			UserInputHandler.clearAllInput();
 
 			keyArray = new string[] { "RIGHT", "RIGHT", "0" };
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(29, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
+
 			keyArray = new string[] { "RIGHT", "RIGHT", "LEFT", "5"};
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(14, UserInputHandler.SelectFromMenu(title, menuOptions, end));
+
+			UserInputHandler.clearAllInput();
 
 			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "8", "LEFT", "6"};
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(25, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
+
 			keyArray = new string[] { "RIGHT", "RIGHT", "RIGHT", "8", "ESCAPE"};
 			KeyBoardInputSimulator.AddKeyBoardInput(keyArray);
 			Assert.AreEqual(-1, UserInputHandler.SelectFromMenu(title, menuOptions, end));
 
+			UserInputHandler.clearAllInput();
 		}
     }
 }
