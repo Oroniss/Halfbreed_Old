c46a24c3f70b54db98f26dcac5f3aa99562f7e0f
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-12 00:15:49 +1100

    Added some more colors to the palette

--- Halfbreed/Halfbreed/Palette.cs
+++ Halfbreed/Halfbreed/Palette.cs
@@ -6,11 +6,24 @@ namespace Halfbreed
 	{
 		// TODO: Update with earlier values.
 		// TODO: Remove references to built in RLColors in consoles, etc.
+		// Basics
 		public static readonly RLColor BLACK = new RLColor(0, 0, 0);
 		public static readonly RLColor WHITE = new RLColor(255, 255, 255);
-		public static readonly RLColor WOODBROWN = new RLColor(173, 119, 74);
-		public static readonly RLColor DARKWOODBROWN = new RLColor(122, 84, 52);
-
-
+		// Browns
+		public static readonly RLColor WOODBROWN = new RLColor(205, 155, 29);
+		public static readonly RLColor DARKWOODBROWN = new RLColor(139, 105, 20);
+		public static readonly RLColor REDBROWN = new RLColor(165, 102, 0);
+		public static readonly RLColor DARKBROWN = new RLColor(109, 85, 5);
+		public static readonly RLColor WOODFOG = new RLColor(165, 125, 20);
+		public static readonly RLColor TAN = new RLColor(210, 180, 140);
+		public static readonly RLColor OLDCLOTH = new RLColor(205, 190, 112);
+		public static readonly RLColor FADEDCLOTH = new RLColor(238, 220, 130);
+		// Greens
+		public static readonly RLColor PUTRIDGREEN = new RLColor(193, 255, 193);
+		public static readonly RLColor VILEGREEN = new RLColor(50, 205, 50);
+		// Metals
+		public static readonly RLColor STEELGREY = new RLColor(122, 122, 122);
+		// Oranges
+		public static readonly RLColor DARKORANGE = new RLColor(238, 154, 0);
 	}
 }
