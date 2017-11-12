4662a3fdfcf42fb03bd431b5390b76c6ab9d5c54
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-12 12:06:02 +1100

    Added building blocks for saving, loading and logging.

--- Halfbreed/Halfbreed/GameEngine.cs
+++ Halfbreed/Halfbreed/GameEngine.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections.Generic;
 namespace Halfbreed
 {
-	public static class GameParameters
+	public static class GameEngine
 	{
 		private static int _difficultySetting;
 		private static CharacterClasses _characterClass;
 		private static bool _useAchievements;
+		private static Level _currentLevel;
+		private static string _saveFileName;
+		private static string _logFileName;
 
-		public static void setStartingDifficulty(int difficulty)
+		public static void SetStartingParameters(NewGameParameters startingParameters)
 		{
-			_difficultySetting = difficulty;
-		}
-		public static void setStartingCharacterClass(CharacterClasses characterClass)
-		{
-			_characterClass = characterClass;
-		}
-		public static void setStartingUseAchievements(bool useAchievements)
-		{
-			_useAchievements = useAchievements;
+			_difficultySetting = startingParameters.DifficultySetting;
+			_characterClass = startingParameters.CharacterClass;
+			_useAchievements = startingParameters.UseAchievements;
+
+			string timeString = System.DateTime.Now.ToString("yyMMddHHmmss");
+			string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+			_saveFileName = currentDirectory + "/Saves/HBS" + timeString + ".hbs";
+			_logFileName = currentDirectory + "/Logs/HBL" + timeString + ".hbl";
 		}
 
 		public static int DifficultySetting
@@ -32,6 +34,29 @@ namespace Halfbreed
 		{
 			get { return _useAchievements; }
 		}
+		public static Level CurrentLevel
+		{
+			get { return _currentLevel; }
+		}
 
+		public static void LevelTransition(string newLevelName, int newX, int newY)
+		{
+			_currentLevel = new Level(newLevelName);
+		}
+
+		// TODO: Put these somewhere else
+		public static void SaveGame()
+		{
+			// Game parameters
+			// Current Level
+			
+		}
+
+		public static void LoadGame()
+		{
+			// Game parameters
+			// Current level
+			
+		}
 	}
 }
