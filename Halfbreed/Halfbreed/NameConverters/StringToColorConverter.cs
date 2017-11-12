1b055fdef9de940969bd29e3eb4c10ff4ee24dc2
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-12 12:04:44 +1100

    Added new colors.

--- Halfbreed/Halfbreed/NameConverters/StringToColorConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/StringToColorConverter.cs
@@ -8,10 +8,25 @@ namespace Halfbreed
 		private static Dictionary<string, RLColor> stringToColorDict = new Dictionary<string, RLColor>()
 		{
 			// TODO: Keep this up to date.
-			{"White", Palette.WHITE},
+			// Basic colors
 			{"Black", Palette.BLACK},
+			{"White", Palette.WHITE},
+			// Browns
 			{"Wood Brown", Palette.WOODBROWN},
-			{"Dark Wood Brown", Palette.DARKWOODBROWN}
+			{"Dark Wood Brown", Palette.DARKWOODBROWN},
+			{"Red Brown", Palette.REDBROWN},
+			{"Dark Brown", Palette.DARKBROWN},
+			{"Wood Fog", Palette.WOODFOG},
+			{"Tan", Palette.TAN},
+			{"Old Cloth", Palette.OLDCLOTH},
+			{"Faded Cloth", Palette.FADEDCLOTH},
+			// Greens
+			{"Putrid Green", Palette.PUTRIDGREEN},
+			{"Vile Green", Palette.VILEGREEN},
+			// Metals
+			{"Steel Grey", Palette.STEELGREY},
+			// Oranges
+			{"Dark Orange", Palette.DARKORANGE}
 		};
 
 		public static RLColor ConvertStringToColor(string colorName)
