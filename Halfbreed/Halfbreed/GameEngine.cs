8334f35f77f0163c3c109d199cd44f3e895a6f57
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-29 16:50:08 +1100

    Pulled a lot of things out to start the switch from serialisation to Database.

--- Halfbreed/Halfbreed/GameEngine.cs
+++ Halfbreed/Halfbreed/GameEngine.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-
+﻿
 namespace Halfbreed
 {
 	public static class GameEngine
@@ -11,19 +7,14 @@ namespace Halfbreed
 		private static CharacterClasses _characterClass;
 		private static bool _useAchievements;
 		private static Level _currentLevel;
-		private static string _saveFileName;
-		private static string _logFileName;
+		private static int _gameId;
 
 		public static void SetStartingParameters(NewGameParameters startingParameters)
 		{
 			_difficultySetting = startingParameters.DifficultySetting;
 			_characterClass = startingParameters.CharacterClass;
 			_useAchievements = startingParameters.UseAchievements;
-
-			string timeString = System.DateTime.Now.ToString("yyMMddHHmmss");
-			string currentDirectory = System.IO.Directory.GetCurrentDirectory();
-			_saveFileName = currentDirectory + "/Saves/HBS" + timeString + ".hbs";
-			_logFileName = currentDirectory + "/Logs/HBL" + timeString + ".hbl";
+			_gameId = startingParameters.GameId;
 		}
 
 		public static int DifficultySetting
@@ -48,30 +39,15 @@ namespace Halfbreed
 			_currentLevel = new Level(newLevelName);
 		}
 
-		// TODO: Put these somewhere else
-		public static void SaveGame()
+		public static SaveGameSummary GenerateSaveSummary()
 		{
-			IFormatter saveFormatter = new BinaryFormatter();
-			Stream saveStream = new FileStream(_saveFileName, FileMode.Create, FileAccess.Write);
-			NewGameParameters parameters = new NewGameParameters(_difficultySetting, _characterClass, _useAchievements);
-			saveFormatter.Serialize(saveStream, parameters);
-			saveStream.Flush();
-			saveStream.Close();
-
-			// Current Level
-			
+			// TODO: Fix once level title goes in.
+			SaveGameSummary summary = new SaveGameSummary(_gameId, _difficultySetting, _characterClass, _useAchievements,
+														  "Wall Market", true, System.DateTime.Now);
+			return summary;
 		}
 
-		public static void LoadGame(string loadFileName)
-		{
-			IFormatter loadFormatter = new BinaryFormatter();
-			Stream loadStream = new FileStream(loadFileName, FileMode.Open, FileAccess.Read);
-			NewGameParameters parameters = (NewGameParameters)loadFormatter.Deserialize(loadStream);
-			loadStream.Close();
-			SetStartingParameters(parameters);
 
-			// Current Level
-		}
 
 	}
 
