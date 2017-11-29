5a9bd164a49d668ee93d10904d3dbc30dc378d53
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-29 16:54:30 +1100

    Also rebuilt to swap from serialisation to database.

--- Halfbreed/Halfbreed/Tests/DatabaseConnectionTests.cs
+++ Halfbreed/Halfbreed/Tests/DatabaseConnectionTests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 namespace Halfbreed
 {
 	[TestFixture]
-	public class SerializationTests
+	public class DatabaseConnectionTests
 	{
 		
 		public void TestReadWriteSummaryFile()
@@ -11,15 +11,6 @@ namespace Halfbreed
 			// TODO: Rewrite all these based on DB connection instead of serialisation.
 			// TODO: This now works, so need to make sure it doesn't overwrite the actual save file.
 
-			DatabaseConnection.SetRootPathForTests(TestContext.CurrentContext.TestDirectory);
-
-			string save1Path = TestContext.CurrentContext.TestDirectory + "testSave1";
-			SaveGameSummary save1 = new SaveGameSummary(1, CharacterClasses.FIGHTER, true, 0, System.DateTime.Now,
-			                                            "Wall Market Square");
-			List<SaveGameSummary> test1List = new List<SaveGameSummary>() { save1 };
-			DatabaseConnection.WriteSaveSummary(test1List);
-
-			Assert.AreEqual(test1List, DatabaseConnection.ReadSaveSummary());
 
 		}
 	}
