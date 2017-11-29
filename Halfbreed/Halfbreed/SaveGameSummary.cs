ed9c2ba0ec6e6acff009d256dcfca3347488306e
jeffrey <jeffrey.fisher@anu.edu.au>
2017-11-29 16:53:47 +1100

    * ReaderWriter.cs:
    * SaveGameSummary.cs: Pulled save game summary into a separate file.

--- Halfbreed/Halfbreed/SaveGameSummary.cs
+++ Halfbreed/Halfbreed/SaveGameSummary.cs
@@ -1,7 +1,40 @@
 ﻿using System;
+
 namespace Halfbreed
 {
 	public struct SaveGameSummary
 	{
+		public int GameId;
+		public int DifficultySetting;
+		public CharacterClasses CharacterClass;
+		public bool UseAchievements;
+		public string CurrentLevelName;
+		public bool StillAlive;
+		public long LastSaveTime;
+
+		public SaveGameSummary(int gameId, int difficultySetting, CharacterClasses characterClass,
+							   bool useAchievements, string currentLevelName, bool stillAlive, DateTime lastSaveTime)
+		{
+			GameId = gameId;
+			DifficultySetting = difficultySetting;
+			CharacterClass = characterClass;
+			UseAchievements = useAchievements;
+			CurrentLevelName = currentLevelName;
+			StillAlive = stillAlive;
+			LastSaveTime = ((DateTimeOffset)lastSaveTime).ToUnixTimeSeconds();
+		}
+
+		public SaveGameSummary(int gameId, int difficultySetting, CharacterClasses characterClass,
+					   bool useAchievements, string currentLevelName, bool stillAlive, long lastSaveTime)
+		{
+			GameId = gameId;
+			DifficultySetting = difficultySetting;
+			CharacterClass = characterClass;
+			UseAchievements = useAchievements;
+			CurrentLevelName = currentLevelName;
+			StillAlive = stillAlive;
+			LastSaveTime = lastSaveTime;
+		}
+
 	}
 }
