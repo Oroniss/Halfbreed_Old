38361251bcc7e23ea1af15d87341b05bceb79a53
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-04 10:29:21 +1100

    Specified the integer for each character class.

--- Halfbreed/Halfbreed/Data/CharacterClasses.cs
+++ Halfbreed/Halfbreed/Data/CharacterClasses.cs
@@ -1,18 +1,17 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
 	public enum CharacterClasses
 	{
-		CLERIC,
-		FIGHTER,
-		MAGE,
-		THIEF,
-		BARD,
-		BLACKGUARD,
-		DRUID,
-		NECROMANCER,
-		PALADIN,
-		RANGER,
-		DRAGONLORD
+		CLERIC=0,
+		FIGHTER=1,
+		MAGE=2,
+		THIEF=3,
+		BARD=4,
+		BLACKGUARD=5,
+		DRUID=6,
+		NECROMANCER=7,
+		PALADIN=8,
+		RANGER=9,
+		DRAGONLORD=10
 	}
 }
