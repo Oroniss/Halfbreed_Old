dbe4b9b6c3f23aa988c964069591d140085e41e6
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-04 10:30:08 +1100

    Added the Select Load Game function.

--- Halfbreed/Halfbreed/LoadGameMenus.cs
+++ Halfbreed/Halfbreed/LoadGameMenus.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace Halfbreed
 {
-	public class LoadGameMenus
+	public static class LoadGameMenus
 	{
-		public LoadGameMenus()
+
+		public static void LoadGame(int gameId)
 		{
+
 		}
+
+		public static int SelectSavedGame()
+		{
+			List<SaveGameSummary> currentSaves = DatabaseConnection.GetSaveGameSummaries();
+			List<string> saveStrings = new List<string>();
+			foreach (SaveGameSummary saveSummary in currentSaves)
+				saveStrings.Add(saveSummary.ToString());
+			
+			string menuText = "Select your champion";
+			if (currentSaves.Count == 0)
+				menuText = "Sorry, there are no surviving champions";
+
+			int selection = UserInputHandler.SelectFromMenu(menuText, saveStrings, "Escape to Cancel");
+
+			if (selection == -1)
+				return -1;
+
+			return currentSaves[selection].GameId;
+		}
+	
+	
 	}
 }
