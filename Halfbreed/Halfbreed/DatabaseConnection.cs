9901fc813c26f53d5bf0ec222f4ad6ac5330c1c6
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-04 10:28:45 +1100

    Minor fix.

---
+++ Halfbreed/Halfbreed/DatabaseConnection.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Data;
+using SQLite.Net;
+using System.Collections.Generic;
+using System;
+namespace Halfbreed
+{
+	public static class DatabaseConnection
+	{
+		private static string _DatabaseLocation = Directory.GetCurrentDirectory() + "/Halfbreed.db";
+		private static SQLiteConnection _connection;
+
+		public static void SetupDirectoriesAndFiles()
+		{
+
+		}
+
+		public static int GenerateNextGameId()
+		{
+			return 0;
+		}
+
+		public static void GetSaveGameSummaries()
+		{
+			SQLite.Net.Platform.Generic.SQLitePlatformGeneric platform = new SQLite.Net.Platform.Generic.SQLitePlatformGeneric();
+			_connection = new SQLiteConnection(platform, _DatabaseLocation);
+
+
+			_connection.Close();
+		}
+
+
+
+		public static void SwitchToTestDatabase()
+		{
+
+		}
+	}
+
+}
