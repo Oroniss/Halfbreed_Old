bf4f55b336c1a30e98826154e0665dccf1e93faa
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-05 17:42:26 +1100

    Added test for generate id, get save summaries and insert new save summary.

--- Halfbreed/Halfbreed/Tests/DatabaseConnectionTests.cs
+++ Halfbreed/Halfbreed/Tests/DatabaseConnectionTests.cs
@@ -1,17 +1,55 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
+
 namespace Halfbreed
 {
 	[TestFixture]
 	public class DatabaseConnectionTests
 	{
-		
-		public void TestReadWriteSummaryFile()
+		[SetUp] // In visual studio this is [TestInitialize]
+		public void TestSetUp()
 		{
-			// TODO: Rewrite all these based on DB connection instead of serialisation.
-			// TODO: This now works, so need to make sure it doesn't overwrite the actual save file.
+			string TestDBLocation = TestContext.CurrentContext.TestDirectory;
+			Assert.True(DatabaseConnection.CopyAndSwitchToTestDatabase(TestDBLocation));
+		}
 
+		[Test]
+		public void SaveSummaryFileMethods()
+		{
+			int nextId = DatabaseConnection.GenerateNextGameId();
+			Assert.AreEqual(3, nextId);
+
+			List<SaveGameSummary> currentSaves = DatabaseConnection.GetSaveGameSummaries();
+
+			Assert.AreEqual(2, currentSaves.Count);
+			Assert.AreEqual(CharacterClasses.FIGHTER, currentSaves[0].CharacterClass);
+			Assert.AreEqual(1, currentSaves[0].DifficultySetting);
+			Assert.IsTrue(currentSaves[0].UseAchievements);
+			Assert.AreEqual(CharacterClasses.THIEF, currentSaves[1].CharacterClass);
+			Assert.IsFalse(currentSaves[1].UseAchievements);
+
+			SaveGameSummary newSaveGameSummary = new SaveGameSummary(nextId, 3, CharacterClasses.PALADIN,
+																	 true, "Wall Market", true, 1200000000);
+
+			DatabaseConnection.InsertNewSaveGameSummary(newSaveGameSummary);
+
+			currentSaves = DatabaseConnection.GetSaveGameSummaries();
+
+			Assert.AreEqual(3, currentSaves.Count);
+			Assert.AreEqual(CharacterClasses.PALADIN, currentSaves[2].CharacterClass);
+			Assert.AreEqual(3, currentSaves[2].DifficultySetting);
+
+			nextId = DatabaseConnection.GenerateNextGameId();
+
+			Assert.AreEqual(4, nextId);
 
 		}
+
+		[TearDown] // In visual studio this is [TestCleanUp]
+		public void TestTearDown()
+		{
+			DatabaseConnection.RemoveTestDb();
+		}
+
 	}
 }
