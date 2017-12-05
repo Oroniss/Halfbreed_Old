9b202575165c81822495a5e297f5bf9b098709d7
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-06 10:26:30 +1100

    Added tests for Write Save Game - only tests the summary part at this stage.

--- Halfbreed/Halfbreed/Tests/SaveDatabaseConnectionTests.cs
+++ Halfbreed/Halfbreed/Tests/SaveDatabaseConnectionTests.cs
@@ -4,22 +4,22 @@ using NUnit.Framework;
 namespace Halfbreed
 {
 	[TestFixture]
-	public class DatabaseConnectionTests
+	public class SaveDatabaseConnectionTests
 	{
 		[SetUp] // In visual studio this is [TestInitialize]
 		public void TestSetUp()
 		{
 			string TestDBLocation = TestContext.CurrentContext.TestDirectory;
-			Assert.True(DatabaseConnection.CopyAndSwitchToTestDatabase(TestDBLocation));
+			Assert.True(SaveDatabaseConnection.CopyAndSwitchToTestDatabase(TestDBLocation));
 		}
 
 		[Test]
 		public void SaveSummaryFileMethods()
 		{
-			int nextId = DatabaseConnection.GenerateNextGameId();
+			int nextId = SaveDatabaseConnection.GenerateNextGameId();
 			Assert.AreEqual(3, nextId);
 
-			List<SaveGameSummary> currentSaves = DatabaseConnection.GetSaveGameSummaries();
+			List<SaveGameSummary> currentSaves = SaveDatabaseConnection.GetSaveGameSummaries();
 
 			Assert.AreEqual(2, currentSaves.Count);
 			Assert.AreEqual(CharacterClasses.FIGHTER, currentSaves[0].CharacterClass);
@@ -31,24 +31,50 @@ namespace Halfbreed
 			SaveGameSummary newSaveGameSummary = new SaveGameSummary(nextId, 3, CharacterClasses.PALADIN,
 																	 true, "Wall Market", true, 1200000000);
 
-			DatabaseConnection.InsertNewSaveGameSummary(newSaveGameSummary);
+			SaveDatabaseConnection.InsertNewSaveGameSummary(newSaveGameSummary);
 
-			currentSaves = DatabaseConnection.GetSaveGameSummaries();
+			currentSaves = SaveDatabaseConnection.GetSaveGameSummaries();
 
 			Assert.AreEqual(3, currentSaves.Count);
 			Assert.AreEqual(CharacterClasses.PALADIN, currentSaves[2].CharacterClass);
 			Assert.AreEqual(3, currentSaves[2].DifficultySetting);
 
-			nextId = DatabaseConnection.GenerateNextGameId();
+			nextId = SaveDatabaseConnection.GenerateNextGameId();
 
 			Assert.AreEqual(4, nextId);
 
 		}
 
+		[Test]
+		public void TestReadAndWriteSaveGame()
+		{
+			List<SaveGameSummary> currentSaves = SaveDatabaseConnection.GetSaveGameSummaries();
+
+			Assert.AreEqual("Wall Market", currentSaves[0].CurrentLevelName);
+			Assert.IsTrue(currentSaves[0].StillAlive);
+			Assert.AreEqual(1000000000, currentSaves[0].LastSaveTime);
+
+			SaveGameSummary updatedSave = new SaveGameSummary(currentSaves[0].GameId,
+															  currentSaves[0].DifficultySetting,
+															  currentSaves[0].CharacterClass,
+															  currentSaves[0].UseAchievements,
+															  "Outcasts End",
+															  true,
+															  1200000000);
+
+			SaveDatabaseConnection.WriteSaveGame(updatedSave, new object());
+
+			currentSaves = SaveDatabaseConnection.GetSaveGameSummaries();
+
+			Assert.AreEqual("Outcasts End", currentSaves[0].CurrentLevelName);
+			Assert.IsTrue(currentSaves[0].StillAlive);
+			Assert.AreEqual(1200000000, currentSaves[0].LastSaveTime);
+		}
+
 		[TearDown] // In visual studio this is [TestCleanUp]
 		public void TestTearDown()
 		{
-			DatabaseConnection.RemoveTestDb();
+			SaveDatabaseConnection.RemoveTestDb();
 		}
 
 	}
