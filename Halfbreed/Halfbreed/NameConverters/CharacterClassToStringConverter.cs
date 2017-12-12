c1f8226a1d3bf443d1a0f791cdf938e96abaa763
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-13 10:43:35 +1100

    Added lower case string conversion.

--- Halfbreed/Halfbreed/NameConverters/CharacterClassToStringConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/CharacterClassToStringConverter.cs
@@ -1,23 +1,34 @@
 ﻿using System.Collections.Generic;
+
 namespace Halfbreed
 {
 	public class CharacterClassToStringConverter
 	{
-		// TODO: Add upper case versions and the reverse.
 		private static Dictionary<string, CharacterClasses> _stringToCharacterClass =
 			new Dictionary<string, CharacterClasses>
 		{
 			{"Cleric", CharacterClasses.CLERIC},
+			{"cleric", CharacterClasses.CLERIC},
 			{"Fighter", CharacterClasses.FIGHTER},
+			{"fighter", CharacterClasses.FIGHTER},
 			{"Mage", CharacterClasses.MAGE},
+			{"mage", CharacterClasses.MAGE},
 			{"Thief", CharacterClasses.THIEF},
+			{"thief", CharacterClasses.THIEF},
 			{"Bard", CharacterClasses.BARD},
+			{"bard", CharacterClasses.BARD},
 			{"Blackguard", CharacterClasses.BLACKGUARD},
+			{"blackguard", CharacterClasses.BLACKGUARD},
 			{"Druid", CharacterClasses.DRUID},
+			{"druid", CharacterClasses.DRUID},
 			{"Necromancer", CharacterClasses.NECROMANCER},
+			{"necromancer", CharacterClasses.NECROMANCER},
 			{"Paladin", CharacterClasses.PALADIN},
+			{"paladin", CharacterClasses.PALADIN},
 			{"Ranger", CharacterClasses.RANGER},
-			{"Dragonlord", CharacterClasses.DRAGONLORD}
+			{"ranger", CharacterClasses.RANGER},
+			{"Dragonlord", CharacterClasses.DRAGONLORD},
+			{"dragonlord", CharacterClasses.DRAGONLORD}
 		};
 
 		private static Dictionary<CharacterClasses, string> _characterClassToString =
