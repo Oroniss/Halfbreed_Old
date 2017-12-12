f548ee292b94d74e7097720aadcca467f761a661
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-13 10:40:47 +1100

    Update

---
+++ Halfbreed/Halfbreed/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace Halfbreed
+{
+	public static class Entity
+	{
+		private static Dictionary<int, List<int>> _components = new Dictionary<int, List<int>>();
+
+	}
+}
