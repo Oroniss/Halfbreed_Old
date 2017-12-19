e52e17fe9fefcddbd84d1f1607e6054d398e3c68
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-19 13:24:55 +1100

    Added methods for setup and cleanup - not sure whether they'll be needed though.

--- Halfbreed/Halfbreed/Entities/Component.cs
+++ Halfbreed/Halfbreed/Entities/Component.cs
@@ -4,11 +4,13 @@
 	{
 		protected ComponentTypes _componentType;
 		private int _entityId;
+		private bool _destroy;
 
 		public Component(int entityId)
 		{
 			_componentType = ComponentTypes.UNDEFINED;
 			_entityId = entityId;
+			_destroy = false;
 		}
 
 		public ComponentTypes ComponentType
@@ -19,6 +21,28 @@
 		public int EntityId
 		{
 			get { return _entityId; }
+		}
+
+		public virtual void Setup()
+		{
+			// Anything that needs to happen after initialisation, but before usage.
+		}
+
+		public virtual void CleanUp()
+		{
+			// Anything that needs to happen before destruction.
+			_destroy = true;
+		}
+
+		public virtual void Destroy()
+		{
+			// Anything that needs to happen when destroyed.
+			if (!_destroy)
+				ErrorLogger.AddDebugText("Tried to destroy component before cleanup called");
+			else
+			{
+				
+			}
 		}
 
 	}
