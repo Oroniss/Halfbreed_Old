6f863d334ea630621185e489a17de7cc818993c5
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-19 13:25:41 +1100

    Added tests for Display Component.

--- Halfbreed/Halfbreed/Tests/ComponentTests.cs
+++ Halfbreed/Halfbreed/Tests/ComponentTests.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+//using RLNET;
+
 namespace Halfbreed
 {
 	[TestFixture]
@@ -33,6 +36,38 @@ namespace Halfbreed
 
 			Assert.AreEqual(-1, testComponent.XLoc);
 			Assert.AreEqual(-1, testComponent.YLoc);
+		}
+
+		[Test]
+		public void TestDisplayComponent()
+		{
+			DisplayComponent testComponent1 = new DisplayComponent(0, 'a', "Steel Grey", DisplayLayer.MINION);
+
+			Assert.AreEqual('a', testComponent1.DisplayCharacter);
+			Assert.AreEqual("Steel Grey", testComponent1.FGColorName);
+			Assert.AreEqual(Palette.STEELGREY, testComponent1.FGColor);
+
+			testComponent1.FGColorName = "Black";
+
+			Assert.AreEqual(Palette.BLACK, testComponent1.FGColor);
+
+			DisplayComponent testComponent2 = new DisplayComponent(1, 'x', "White", DisplayLayer.DOOR);
+			DisplayComponent testComponent3 = new DisplayComponent(2, 'x', "White", DisplayLayer.PLAYER);
+			DisplayComponent testComponent4 = new DisplayComponent(3, 'x', "White", DisplayLayer.HARVESTABLE);
+			DisplayComponent testComponent5 = new DisplayComponent(4, 'x', "White", DisplayLayer.ITEM);
+
+			List<DisplayComponent> componentList = new List<DisplayComponent>();
+
+			componentList.Add(testComponent1);
+			componentList.Add(testComponent2);
+			componentList.Add(testComponent3);
+			componentList.Add(testComponent4);
+			componentList.Add(testComponent5);
+
+			componentList.Sort();
+
+			Assert.AreEqual(2, componentList[0].EntityId);
+			Assert.AreEqual(3, componentList[4].EntityId);
 		}
 
 	}
