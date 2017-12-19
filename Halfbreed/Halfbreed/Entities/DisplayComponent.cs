e85b133e9c0269f02a6d9e95612e6c15f88f3411
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-19 13:25:14 +1100

    Created to store the visual information for display.

--- Halfbreed/Halfbreed/Entities/DisplayComponent.cs
+++ Halfbreed/Halfbreed/Entities/DisplayComponent.cs
@@ -1,10 +1,55 @@
 ﻿using System;
+using RLNET;
+
 namespace Halfbreed
 {
-	public class DisplayComponent
+	public class DisplayComponent : Component, IComparable<DisplayComponent>
 	{
-		public DisplayComponent()
+		private char _displayCharacter;
+		private string _fgColorName;
+		private RLColor _fgColor;
+		private DisplayLayer _layer;
+
+		public DisplayComponent(int entityId, char character, string fgColorName, DisplayLayer layer)
+			: base(entityId)
 		{
+			_componentType = ComponentTypes.DISPLAY;
+			_displayCharacter = character;
+			_fgColorName = fgColorName;
+			_fgColor = StringToColorConverter.ConvertStringToColor(fgColorName);
+			_layer = layer;
 		}
+
+		public char DisplayCharacter
+		{
+			get { return _displayCharacter; }
+			set { _displayCharacter = value; }
+		}
+
+		public string FGColorName
+		{
+			get { return _fgColorName; }
+			set
+			{
+				_fgColorName = value;
+				_fgColor = StringToColorConverter.ConvertStringToColor(value);}
+			}
+
+		public RLColor FGColor
+		{
+			get { return _fgColor; }
+		}
+
+		public DisplayLayer Layer
+		{
+			get { return _layer; }
+			set { _layer = value; }
+		}
+
+		public int CompareTo(DisplayComponent other)
+		{
+			return ((int)_layer).CompareTo((int)other.Layer);
+		}
+
 	}
 }
