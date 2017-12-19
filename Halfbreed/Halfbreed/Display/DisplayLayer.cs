f6c7363d766381ecc43dddea7e6ae9ec22e5ca8b
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-19 13:23:52 +1100

    Updated

---
+++ Halfbreed/Halfbreed/Display/DisplayLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Halfbreed
+{
+	public enum DisplayLayer
+	{
+		MISC = 0,
+		INTERFACE = 1,
+		PLAYER = 2,
+		MINION = 3,
+		ACTOR = 4,
+		DOOR = 5,
+		TRAP = 6,
+		ITEM = 7,
+		HARVESTABLE = 8,
+		FURNISHING = 9,
+		BACKGROUND = 10
+	}
+}
