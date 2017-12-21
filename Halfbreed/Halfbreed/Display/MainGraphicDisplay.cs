74486ce3497cc3fcce5c7c3bf3e6a5833cce0efe
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-21 15:24:42 +1100

    Refactored the setup to make it easier to understand what's happening. Also put the whole thing into a separate namespace.

--- Halfbreed/Halfbreed/Display/MainGraphicDisplay.cs
+++ Halfbreed/Halfbreed/Display/MainGraphicDisplay.cs
@@ -1,27 +1,36 @@
 using RLNET;
+using Halfbreed.Display;
 
 namespace Halfbreed
 {
     public static class MainGraphicDisplay
     {
-		// TODO: See if this can be cleaned up a little.
+		// TODO: Eventually these should probably go in a settings file or similar.
 		private static int _backConsoleWidth = 160;
 		private static int _backConsoleHeight = 80;
 		private static BackConsole _backConsole = new BackConsole(_backConsoleWidth, _backConsoleHeight);
 
-		private static int _menuConsoleOffset = 2;
-		private static MenuConsole _menuConsole = new MenuConsole(_backConsoleWidth - 2 * _menuConsoleOffset,
-		                                                          _backConsoleHeight - 2 * _menuConsoleOffset,
-		                                                          _menuConsoleOffset,
-		                                                          _menuConsoleOffset, 
+		private static int _menuConsoleWidth = _backConsoleWidth - 4;
+		private static int _menuConsoleHeight = _backConsoleHeight - 4;
+		private static int _menuConsoleXOffset = 2;
+		private static int _menuConsoleYOffset = 2;
+		private static MenuConsole _menuConsole = new MenuConsole(_menuConsoleWidth,
+		                                                          _menuConsoleHeight,
+		                                                          _menuConsoleXOffset,
+		                                                          _menuConsoleYOffset, 
 		                                                          Palette.GetColor(Colors.LIGHTGREY), 
 		                                                          _backConsole);
-
+		
 		private static int _mapConsoleWidth = 80;
 		private static int _mapConsoleHeight = 80;
-		private static int _mapConsoleOffset = 0;
-		private static MapConsole _mapConsole = new MapConsole(_mapConsoleWidth, _mapConsoleHeight, 0, 0, 
-		                                                       Palette.GetColor(Colors.BLACK), _backConsole);
+		private static int _mapConsoleXOffset = 0;
+		private static int _mapConsoleYOffset = 0;
+		private static MapConsole _mapConsole = new MapConsole(_mapConsoleWidth, 
+		                                                       _mapConsoleHeight,
+															   _mapConsoleXOffset, 
+		                                                       _mapConsoleYOffset, 
+		                                                       Palette.GetColor(Colors.BLACK), 
+		                                                       _backConsole);
 
 		public static bool IsDirty
 		{
