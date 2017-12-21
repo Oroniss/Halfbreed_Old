dffcdb9ba65830a072d8ff085db04e6d7615cdff
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-21 15:00:46 +1100

    Updated to reflect converter refactoring.

--- Halfbreed/Halfbreed/Tests/NameConverterTests.cs
+++ Halfbreed/Halfbreed/Tests/NameConverterTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using System;
 using RLNET;
 
@@ -11,43 +11,45 @@ namespace Halfbreed
 		[Test]
 		public void TestKeyToStringConverter()
 		{
-			Assert.IsFalse(NameConverter.KeyToStringConverter.checkKeyIsValid(RLKey.Tab));
-			Assert.IsTrue(NameConverter.KeyToStringConverter.checkKeyIsValid(RLKey.Escape));
-
-			Assert.AreEqual("ESCAPE", NameConverter.KeyToStringConverter.convertKeyToString(RLKey.Escape));
-			Assert.AreEqual("LEFT", NameConverter.KeyToStringConverter.convertKeyToString(RLKey.Left));
-			Assert.AreEqual("RIGHT", NameConverter.KeyToStringConverter.convertKeyToString(RLKey.Right));
+			Assert.AreEqual("ESCAPE", EnumConverter.ConvertEnumToString(RLKey.Escape));
+			Assert.AreEqual("LEFT", EnumConverter.ConvertEnumToString(RLKey.Left));
+			Assert.AreEqual("RIGHT", EnumConverter.ConvertEnumToString(RLKey.Right));
 		}
 
 		[Test]
 		public void TestCharacterClassToStringConverter()
 		{
 			Assert.AreEqual(CharacterClasses.BARD,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("Bard"));
+			                EnumConverter.ConvertStringToCharacterClass("Bard"));
 			Assert.AreEqual(CharacterClasses.PALADIN,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("Paladin"));
+							EnumConverter.ConvertStringToCharacterClass("Paladin"));
 			Assert.AreEqual(CharacterClasses.FIGHTER,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("Fighter"));
+							EnumConverter.ConvertStringToCharacterClass("Fighter"));
 			Assert.AreEqual(CharacterClasses.DRAGONLORD,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("Dragonlord"));
+							EnumConverter.ConvertStringToCharacterClass("Dragonlord"));
 			Assert.AreEqual(CharacterClasses.RANGER,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("ranger"));
+							EnumConverter.ConvertStringToCharacterClass("ranger"));
 			Assert.AreEqual(CharacterClasses.DRUID,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("druid"));
+							EnumConverter.ConvertStringToCharacterClass("druid"));
 			Assert.AreEqual(CharacterClasses.THIEF,
-							NameConverter.CharacterClassToStringConverter.ConvertStringToCharacterClass("thief"));
+							EnumConverter.ConvertStringToCharacterClass("thief"));
 		}
 
 		[Test]
 		public void TestStringToColorConverter()
 		{
 			// TODO: Add some more here
-			Assert.AreEqual(Colors.BLACK, NameConverter.StringToColorConverter.ConvertStringToColor("Black"));
-			Assert.AreEqual(Colors.WHITE, NameConverter.StringToColorConverter.ConvertStringToColor("White"));
-			Assert.AreEqual(Colors.WOODBROWN, NameConverter.StringToColorConverter.ConvertStringToColor("Wood Brown"));
-			Assert.AreEqual(Colors.DARKWOODBROWN, NameConverter.StringToColorConverter.ConvertStringToColor("Dark Wood Brown"));
+			Assert.AreEqual(Colors.BLACK, EnumConverter.ConvertStringToColor("Black"));
+			Assert.AreEqual(Colors.WHITE, EnumConverter.ConvertStringToColor("White"));
+			Assert.AreEqual(Colors.WOODBROWN, EnumConverter.ConvertStringToColor("Wood Brown"));
+			Assert.AreEqual(Colors.DARKWOODBROWN, EnumConverter.ConvertStringToColor("Dark Wood Brown"));
 
-			Assert.AreEqual(Enum.GetNames(typeof(Colors)).Length, NameConverter.StringToColorConverter.NumberOfColors);
+		}
+
+		[Test]
+		public void CheckDictionaryConsistency()
+		{
+			// TODO: Write a function on the converter that validates everything has the right number of entries.
 		}
 
 	}
