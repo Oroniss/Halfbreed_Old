72f0a3fb2a21255bb02db8f0016cc24f45a70688
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-22 01:08:25 +1100

    Added some tests for the Palette class.

---
+++ Halfbreed/Halfbreed/Tests/DataTests.cs
@@ -0,0 +1,22 @@
+﻿using RLNET;
+using System;
+using NUnit.Framework;
+
+namespace Halfbreed
+{
+	[TestFixture]
+	public class DataTests
+	{
+		[Test]
+		public void PaletteTest()
+		{
+			Assert.AreEqual(new RLColor(190, 190, 190), Palette.GetColor(Colors.LIGHTGREY));
+			Assert.AreEqual(new RLColor(255, 255, 255), Palette.GetColor(Colors.WHITE));
+			Assert.AreEqual(new RLColor(193, 255, 193), Palette.GetColor(Colors.PUTRIDGREEN));
+
+			Assert.AreEqual(Enum.GetValues(typeof(Colors)).Length, Palette.NumberOfColors);
+
+		}
+
+	}
+}
