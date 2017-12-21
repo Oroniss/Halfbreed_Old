b8e7c4dcaf7c364f51f4ae6174c1bd6bc6906c6e
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-21 15:00:23 +1100

    Did another major refactor of the converter - a single point of entry with a generic method to convert an enum to strings. And specific methods to convert strings to individual enum types - the alternative generic method was really cumbersome.

---
+++ Halfbreed/Halfbreed/NameConverters/EnumConverter.cs
@@ -0,0 +1,37 @@
+﻿using Halfbreed.Converters;
+
+namespace Halfbreed
+{
+	public static class EnumConverter
+	{
+		private static KeyToStringConverter KeyToStringConverter = new KeyToStringConverter();
+		private static StringToColorConverter StringToColorConverter = new StringToColorConverter();
+		private static CharacterClassToStringConverter CharacterClassToStringConverter = new CharacterClassToStringConverter();
+
+		public static string ConvertEnumToString(object enumValue)
+		{
+			if(enumValue is RLNET.RLKey)
+			{
+				return KeyToStringConverter.convertKeyToString((RLNET.RLKey)enumValue);
+			}
+
+			if (enumValue is CharacterClasses)
+			{
+				return CharacterClassToStringConverter.ConvertCharacterClassToString(
+					(CharacterClasses)enumValue);
+			}
+
+			return "";
+		}
+
+		public static Colors ConvertStringToColor(string inputValue)
+		{
+			return StringToColorConverter.ConvertStringToColor(inputValue);
+		}
+
+		public static CharacterClasses ConvertStringToCharacterClass(string inputValue)
+		{
+			return CharacterClassToStringConverter.ConvertStringToCharacterClass(inputValue);
+		}
+	}
+}
