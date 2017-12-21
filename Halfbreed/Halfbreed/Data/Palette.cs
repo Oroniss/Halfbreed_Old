a2b9935cd8956f7e4d436b7ad64da84eadc069e2
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-21 13:44:35 +1100

    Refactored the Palette class and associated interactions - now all goes through a Colors enum. Palette class is responsible for converting them to RL Colors. Only Palette and the Consoles actually care about RLColors now.

--- Halfbreed/Halfbreed/Data/Palette.cs
+++ Halfbreed/Halfbreed/Data/Palette.cs
@@ -1,28 +1,67 @@
 ﻿using RLNET;
+using System.Collections.Generic;
 
 namespace Halfbreed
 {
 	public static class Palette
 	{
+		private static readonly Dictionary<Colors, RLColor> _colors = new Dictionary<Colors, RLColor>()
+		{
+			// Basics
+			{Colors.BLACK, new RLColor(0, 0, 0)},
+			{Colors.WHITE, new RLColor(255, 255, 255)},
+			// Browns
+			{Colors.WOODBROWN, new RLColor(205, 155, 29)},
+			{Colors.DARKWOODBROWN, new RLColor(139, 105, 20)},
+			{Colors.REDBROWN, new RLColor(165, 102, 0)},
+			{Colors.DARKBROWN, new RLColor(109, 85, 5)},
+			{Colors.WOODFOG, new RLColor(165, 125, 20)},
+			{Colors.TAN, new RLColor(210, 180, 140)},
+			{Colors.OLDCLOTH, new RLColor(205, 190, 112)},
+			{Colors.FADEDCLOTH, new RLColor(238, 220, 130)},
+			// Greens
+			{Colors.PUTRIDGREEN, new RLColor(193, 255, 193)},
+			{Colors.VILEGREEN, new RLColor(50, 205, 50)},
+			// Metals
+			{Colors.LIGHTGREY, new RLColor(190, 190, 190)},
+			{Colors.STEELGREY, new RLColor(122, 122, 122)},
+			// Oranges
+			{Colors.DARKORANGE, new RLColor(238, 154, 0)}
+		};
+
+		public static RLColor GetColor(Colors colorName)
+		{
+			return _colors[colorName];
+		}
+
+		public static int NumberOfColors
+		{
+			get { return _colors.Count; }
+		}
+			
+	}
+
+	public enum Colors
+	{
 		// Basics
-		public static readonly RLColor BLACK = new RLColor(0, 0, 0);
-		public static readonly RLColor WHITE = new RLColor(255, 255, 255);
+		BLACK,
+		WHITE,
 		// Browns
-		public static readonly RLColor WOODBROWN = new RLColor(205, 155, 29);
-		public static readonly RLColor DARKWOODBROWN = new RLColor(139, 105, 20);
-		public static readonly RLColor REDBROWN = new RLColor(165, 102, 0);
-		public static readonly RLColor DARKBROWN = new RLColor(109, 85, 5);
-		public static readonly RLColor WOODFOG = new RLColor(165, 125, 20);
-		public static readonly RLColor TAN = new RLColor(210, 180, 140);
-		public static readonly RLColor OLDCLOTH = new RLColor(205, 190, 112);
-		public static readonly RLColor FADEDCLOTH = new RLColor(238, 220, 130);
+		WOODBROWN,
+		DARKWOODBROWN,
+		REDBROWN,
+		DARKBROWN,
+		WOODFOG,
+		TAN,
+		OLDCLOTH,
+		FADEDCLOTH,
 		// Greens
-		public static readonly RLColor PUTRIDGREEN = new RLColor(193, 255, 193);
-		public static readonly RLColor VILEGREEN = new RLColor(50, 205, 50);
+		PUTRIDGREEN,
+		VILEGREEN,
 		// Metals
-		public static readonly RLColor LIGHTGREY = new RLColor(190, 190, 190);
-		public static readonly RLColor STEELGREY = new RLColor(122, 122, 122);
+		LIGHTGREY,
+		STEELGREY,
 		// Oranges
-		public static readonly RLColor DARKORANGE = new RLColor(238, 154, 0);
+		DARKORANGE
 	}
 }
