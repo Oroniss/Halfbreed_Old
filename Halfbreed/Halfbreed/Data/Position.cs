c95e1e137c3c9b38d1ecfb19f5de5dae37d4d500
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:19:16 +1100

    Position struct - self explanatory.

--- Halfbreed/Halfbreed/Data/Position.cs
+++ Halfbreed/Halfbreed/Data/Position.cs
@@ -3,5 +3,23 @@ namespace Halfbreed
 {
 	public struct Position
 	{
+		private int _x;
+		private int _y;
+
+		public Position(int x, int y)
+		{
+			_x = x;
+			_y = y;
+		}
+
+		public int X
+		{
+			get { return _x; }
+		}
+
+		public int Y
+		{
+			get { return _y; }
+		}
 	}
 }
