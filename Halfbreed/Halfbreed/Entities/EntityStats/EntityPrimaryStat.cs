4227c87b43052e91d1d5853656ed1ffa91e72b3a
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:23:30 +1100

    Primary stat - agility, might, etc.

--- Halfbreed/Halfbreed/Entities/EntityStats/EntityPrimaryStat.cs
+++ Halfbreed/Halfbreed/Entities/EntityStats/EntityPrimaryStat.cs
@@ -6,11 +6,13 @@ namespace Halfbreed.Entities
 	{
 		private int _baseRating;
 		private int _ratingModifiers;
+		private float _currentMultiplier;
 
 		public EntityPrimaryStat(int baseRating)
 		{
 			_baseRating = baseRating;
 			_ratingModifiers = 0;
+			recalculateMultiplier();
 		}
 
 		public int Rating
@@ -18,22 +20,28 @@ namespace Halfbreed.Entities
 			get { return _baseRating + _ratingModifiers; }
 		}
 
+		public float Multiplier
+		{
+			get { return _currentMultiplier; }
+		}
+
 		public void addModifier(int modifier)
 		{
 			_ratingModifiers += modifier;
+			recalculateMultiplier();
 		}
 
 		public void reset()
 		{
 			_ratingModifiers = 0;
+			recalculateMultiplier();
 		}
 
-		public float Multiplier
+		private void recalculateMultiplier()
 		{
-			get {
 				float chance = (float)((Math.Log(Rating / 100 + 0.8, 2) * 0.4) -
-				                       ((GameEngine.CurrentChapter - 1) * 0.08) + 1);
-				return (float)Math.Round(Math.Min(1 + GameEngine.CurrentAct * 0.4, Math.Max(chance, 1)), 3); }
+					   ((GameEngine.CurrentChapter - 1) * 0.08) + 1);
+				_currentMultiplier = (float)Math.Round(Math.Min(1 + GameEngine.CurrentAct* 0.4, Math.Max(chance, 1)), 3);
 		}
 	}
 }
