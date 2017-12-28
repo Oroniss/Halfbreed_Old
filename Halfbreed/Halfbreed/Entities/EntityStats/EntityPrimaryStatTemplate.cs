c8a1183804f538bb690e15e9bd40708461ec8db5
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:24:33 +1100

    Template for pulling primary stats out of the DB.

--- Halfbreed/Halfbreed/Entities/EntityStats/EntityPrimaryStatTemplate.cs
+++ Halfbreed/Halfbreed/Entities/EntityStats/EntityPrimaryStatTemplate.cs
@@ -1,7 +1,45 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
 	public struct EntityPrimaryStatTemplate
 	{
+		private int _might;
+		private int _agility;
+		private int _mind;
+		private int _willpower;
+		private int _presence;
+
+		public EntityPrimaryStatTemplate(int might, int agility, int mind, int willpower, int presence)
+		{
+			_might = might;
+			_agility = agility;
+			_mind = mind;
+			_willpower = willpower;
+			_presence = presence;
+		}
+
+		public int Might
+		{
+			get { return _might; }
+		}
+
+		public int Agility
+		{
+			get { return _agility; }
+		}
+
+		public int Mind
+		{
+			get { return _mind; }
+		}
+
+		public int Willpower
+		{
+			get { return _willpower; }
+		}
+
+		public int Presence
+		{
+			get { return _presence; }
+		}
 	}
 }
