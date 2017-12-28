c2e4ce9c05f569372da053effccac1e57640a88e
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:25:02 +1100

    Started compiling a list of materials.

--- Halfbreed/Halfbreed/Entities/Materials.cs
+++ Halfbreed/Halfbreed/Entities/Materials.cs
@@ -1,7 +1,29 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
 	public enum Materials
 	{
+		// Woods
+		BIRCH,
+		CEDAR,
+		OAK,
+		ASPEN,
+		ELM,
+		IRONWOOD,
+		DRAGONHORN,
+		DIHYR,
+		// Base metals
+		TIN,
+		IRON,
+		BRONZE,
+		STEEL,
+		EOG,
+		LAEN,
+		ADAMANTIUM,
+		// Precious Metals
+		COPPER,
+		SILVER,
+		GOLD,
+		PLATINUM,
+		MITHRIL
 	}
 }
