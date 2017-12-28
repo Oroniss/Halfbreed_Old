a6e3f40c9f518e5fafb84826abb1f4aa4c0fd18c
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:25:35 +1100

    Deleted - put on entity directly.

--- Halfbreed/Halfbreed/Entities/TraitManager.cs
+++ Halfbreed/Halfbreed/Entities/TraitManager.cs
@@ -1,54 +1,19 @@
 using System.Collections.Generic;
 namespace Halfbreed
 {
-	public static partial class EntityManager
+	public partial class Entity
 	{
-		private static Dictionary<int, List<EntityTraits>> _entityTraits = new Dictionary<int, List<EntityTraits>>();
-
-		public static bool HasTrait(int entityId, EntityTraits trait)
+		public bool HasTrait(int entityId, EntityTraits trait)
 		{
-			if (_entityTraits.ContainsKey(entityId))
-				return _entityTraits[entityId].Contains(trait);
-			else
-			{
-				ErrorLogger.AddDebugText(string.Format("Checked for trait on unknown entity ID: {0}", entityId.ToString()));
-				return false;
-			}
+			return false;
 		}
 
-		public static void AddTrait(int entityId, EntityTraits trait)
+		public void AddTrait(int entityId, EntityTraits trait)
 		{
-			if (_entityTraits.ContainsKey(entityId))
-			{
-				_entityTraits[entityId].Add(trait);
-				// TODO: Should we have an analagous list as per Components?
-				return;
-			}
-			else
-			{
-				ErrorLogger.AddDebugText(string.Format("Tried to add trait on unknown entity ID: {0}", entityId.ToString()));
-				return;
-			}
 		}
 
-		public static void RemoveTrait(int entityId, EntityTraits trait)
+		public void RemoveTrait(int entityId, EntityTraits trait)
 		{
-			if (_entityTraits.ContainsKey(entityId))
-			{
-				if (_entityTraits[entityId].Contains(trait))
-					_entityTraits[entityId].Remove(trait);
-				else
-				{
-					ErrorLogger.AddDebugText(string.Format("Tried to remove trait {0} from entity id {1}" +
-														   " but it was not present.", trait.ToString(), entityId.ToString()));
-					return;
-				}
-			}
-			else
-			{
-				ErrorLogger.AddDebugText(string.Format("Tried to remove trait from unknown entity ID: {0}", entityId.ToString()));
-				return;
-			}
 		}
 	}
 }
