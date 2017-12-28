f78b580a23f8617fd8070cf3ae8fd5dd25f5723a
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-28 11:25:22 +1100

    The different movement modes - still needs more thinking about.

--- Halfbreed/Halfbreed/Entities/Movement/MovementModes.cs
+++ Halfbreed/Halfbreed/Entities/Movement/MovementModes.cs
@@ -1,7 +1,12 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
 	public enum MovementModes
 	{
+		IMMOBILE = 0
+		WALK = 1,
+		CLIMB = 2,
+		FLY = 3,
+		PHASE = 4,
+		TELEPORT = 5
 	}
 }
