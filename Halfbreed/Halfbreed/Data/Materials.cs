c6a08271c7ee8720d0e94732aad183a9cc988273
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-30 10:24:58 +1100

    Cut back to a few necessary starting materials.

--- Halfbreed/Halfbreed/Data/Materials.cs
+++ Halfbreed/Halfbreed/Data/Materials.cs
@@ -3,27 +3,32 @@
 	public enum Materials
 	{
 		// Woods
-		BIRCH,
-		CEDAR,
-		OAK,
-		ASPEN,
-		ELM,
-		IRONWOOD,
-		DRAGONHORN,
-		DIHYR,
+		PINE,
+		//BIRCH,
+		//CEDAR,
+		//OAK,
+		//ASPEN,
+		//ELM,
+		//IRONWOOD,
+		//DRAGONHORN,
+		//DIHYR,
 		// Base metals
 		TIN,
-		IRON,
-		BRONZE,
-		STEEL,
-		EOG,
-		LAEN,
-		ADAMANTIUM,
+		//IRON,
+		//BRONZE,
+		//STEEL,
+		//EOG,
+		//LAEN,
+		//ADAMANTIUM,
 		// Precious Metals
 		COPPER,
-		SILVER,
-		GOLD,
-		PLATINUM,
-		MITHRIL
+		//SILVER,
+		//GOLD,
+		//PLATINUM,
+		//MITHRIL
+		// Leathers
+		FUR,
+		// Cloths
+		HESSION
 	}
 }
