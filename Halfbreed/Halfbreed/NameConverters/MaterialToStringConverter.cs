4c82c733d3164de64edfcf71649b4c79ea6839f4
jeffrey <jeffrey.fisher@anu.edu.au>
2017-12-30 10:24:27 +1100

    Added material to string.

--- Halfbreed/Halfbreed/NameConverters/MaterialToStringConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/MaterialToStringConverter.cs
@@ -1,10 +1,26 @@
-﻿using System;
-namespace Halfbreed
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Converters
 {
 	public class MaterialToStringConverter
 	{
-		public MaterialToStringConverter()
+		private static Dictionary<string, Materials> _stringToMaterials = new Dictionary<string, Materials>
 		{
+			{"Copper", Materials.COPPER},
+			{"Fur", Materials.FUR},
+			{"Hession", Materials.HESSION},
+			{"Pine", Materials.PINE},
+			{"Tin", Materials.TIN}
+		};
+
+		public Materials ConvertStringToMaterial(string inputValue)
+		{
+			return _stringToMaterials[inputValue];
+		}
+
+		public static int NumberOfMaterials
+		{
+			get { return _stringToMaterials.Count; }
 		}
 	}
 }
