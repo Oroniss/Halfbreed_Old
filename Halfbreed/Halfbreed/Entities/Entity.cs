833b94997dc0b522b54518fbbd9bb34c7b54d103
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-03 17:29:02 +1100

    Added back in as an abstract base class - still not happy with it.

--- Halfbreed/Halfbreed/Entities/Entity.cs
+++ Halfbreed/Halfbreed/Entities/Entity.cs
@@ -1,10 +1,47 @@
 ﻿using System;
-namespace Halfbreed
+
+namespace Halfbreed.Entities
 {
-	public class Entity
+	public abstract class Entity : IPosition, IDescription, IComparable
 	{
-		public Entity()
+		protected DisplayLayer _displayLayer;
+		protected Colors _fgColor;
+		protected char _symbol;
+
+		protected int _xLoc;
+		protected int _yLoc;
+
+		public DisplayLayer DisplayLayer
 		{
+			get {return _displayLayer;}
 		}
+
+		public Colors FGColor
+		{
+			get{return _fgColor;}
+		}
+
+		public char Symbol
+		{
+			get { return _symbol; }
+		}
+
+		public abstract string GetDescription();
+
+		public int XLoc
+		{
+			get { return _xLoc; }
+		}
+
+		public int YLoc
+		{
+			get { return _yLoc; }
+		}
+
+		public int CompareTo(object obj)
+		{
+			return _displayLayer.CompareTo(((Entity)obj).DisplayLayer);
+		}
+
 	}
 }
