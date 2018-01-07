60be78ab3186d54606b61d216dd497f8a07f521e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-08 00:22:23 +1100

    Created in preparation - will likely get a bit messy later on.

--- Halfbreed/Halfbreed/Entities/Furnishings/FurnishingFactory.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/FurnishingFactory.cs
@@ -1,10 +1,19 @@
-﻿using System;
-namespace Halfbreed
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Entities
 {
-	public class FurnishingFactory
+	public static class FurnishingFactory
 	{
-		public FurnishingFactory()
+
+		private static Dictionary<string, FurnishingTemplate> _furnishings = new Dictionary<string, FurnishingTemplate>();
+
+		public static Furnishing CreateFurnishing(string furnishingName, Materials material, int xLoc, int yLoc, string[] otherParams)
 		{
+			if (!_furnishings.ContainsKey(furnishingName))
+				_furnishings[furnishingName] = EntityDatabaseConnection.GetFurnishingDetails(furnishingName);
+
+			Furnishing newFurnishing = new Furnishing(furnishingName, material, _furnishings[furnishingName], xLoc, yLoc, otherParams);
+			return newFurnishing;
 		}
 	}
 }
