b7bff628915b189f1c5e16e3154f4875090bd6d4
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-08 00:21:58 +1100

    Created a storage container for the furnishing info coming out of the db.

--- Halfbreed/Halfbreed/Entities/Furnishings/FurnishingTemplate.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/FurnishingTemplate.cs
@@ -1,7 +1,31 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
 	public struct FurnishingTemplate
 	{
+		private string _furnishingName;
+		private char _symbol;
+		private int _volume;
+
+		public FurnishingTemplate(string furnishingName, char symbol, int volume)
+		{
+			_furnishingName = furnishingName;
+			_symbol = symbol;
+			_volume = volume;
+		}
+
+		public string FurnishingName
+		{
+			get { return _furnishingName; }
+		}
+
+		public char Symbol
+		{
+			get { return _symbol; }
+		}
+
+		public int Volume
+		{
+			get { return _volume; }
+		}
 	}
 }
