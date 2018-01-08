430d292fee8cc246077378685fa5eac4a4b36616
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-08 12:39:57 +1100

    Added tests for the dictionary that gets the material properties.
    Actual tests for entities were harder since the class is Abstract.
    Testing these will be done through the derived classes - worked ok for
    furnishings.

--- Halfbreed/Halfbreed/Tests/EntityTests.cs
+++ Halfbreed/Halfbreed/Tests/EntityTests.cs
@@ -9,10 +9,29 @@ namespace Halfbreed
 	public class EntityTests
 	{
 
-		[Test]
-		public void TestEntityDefensiveStats()
+		[SetUp]
+		public void OpenTestDatabases()
 		{
-			
+			EntityDatabaseConnection.SetupTestContext(TestContext.CurrentContext.TestDirectory);
+			EntityDatabaseConnection.openDBConnection();
+			Entities.EntityData.SetupDictionaries();
+		}
+
+		[Test]
+		public void TestMaterialDictionary()
+		{
+			Assert.AreEqual(Enum.GetValues(typeof(Materials)).Length, EntityData.GetNumberOfMaterials());
+
+			Assert.AreEqual(200, EntityData.GetProperties(Materials.COPPER).DefensiveStatTemplate.AcidResistance);
+			Assert.AreEqual(400, EntityData.GetProperties(Materials.HESSION).DefensiveStatTemplate.LightResistance);
+			Assert.AreEqual(10, EntityData.GetProperties(Materials.TIN).Hardness);
+			Assert.AreEqual(Colors.TAN, EntityData.GetProperties(Materials.HESSION).FGColor);
+		}
+
+		[TearDown]
+		public void CloseTestDatabases()
+		{
+			EntityDatabaseConnection.closeDBConnection();
 		}
 
 	}
