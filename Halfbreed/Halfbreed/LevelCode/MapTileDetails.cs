033635ebd5876718503d8ac35e914a0011742e0b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-09 17:28:59 +1100

    Made a class, not a struct. Also moved the static dictionary onto the StaticData class.

--- Halfbreed/Halfbreed/LevelCode/MapTileDetails.cs
+++ Halfbreed/Halfbreed/LevelCode/MapTileDetails.cs
@@ -1,35 +1,21 @@
-using System.Collections.Generic;
-
 namespace Halfbreed
 {
-	public static class MapTileDetails
+
+	public class MapTileDetails
 	{
-		public static Dictionary<TileType, TileDetails> MapTileDict = new Dictionary<TileType, TileDetails>()
-		{
-			// TODO: Eventually this should probably be read in from somewhere.
-			// TODO: At least think about it.
-			{TileType.WOODFLOOR, new TileDetails("Wood Floor", 0, 0, true, "Wood Brown", "Wood Brown")},
-			{TileType.WOODWALL, new TileDetails("Wood Wall", 20, 4, false, "Dark Wood Brown", "Dark Wood Brown")},
-			{TileType.WOODENDEBRIS, new TileDetails("Wooden Debris", 20, 4, false, "Dark Wood Brown", "Dark Wood Brown")}
-		};
+		public readonly string Name;
+		public readonly int Elevation;
+		public readonly MovementModes MoveModes;
+		public readonly bool AllowLOS;
+		public readonly Colors BGColor;
+		public readonly Colors FogColor;
 
-	}
-
-	public struct TileDetails
-	{
-		public string Name;
-		public int Elevation;
-		public int MoveTypes;
-		public bool AllowLOS;
-		public Colors BGColor;
-		public Colors FogColor;
-
-		internal TileDetails(string name, int elevation, int moveTypes, bool allowLOS, string bgColorName,
+		public MapTileDetails(string name, int elevation, MovementModes moveTypes, bool allowLOS, string bgColorName,
 							string fogColorName)
 		{
 			Name = name;
 			Elevation = elevation;
-			MoveTypes = moveTypes;
+			MoveModes = moveTypes;
 			AllowLOS = allowLOS;
 			BGColor = EnumConverter.ConvertStringToColor(bgColorName);
 			FogColor = EnumConverter.ConvertStringToColor(fogColorName);
