e5802334780a51e229a8806207dc5845a17a384f
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-11 14:12:09 +1100

    Replaces a map tile - only for furnishings.

--- Halfbreed/Halfbreed/Entities/Components/TileComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/TileComponent.cs
@@ -1,10 +1,20 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
-	public class TileComponent
+	public class TileComponent:Component
 	{
-		public TileComponent()
+		private MapTileDetails _tileDetails;
+
+		public TileComponent(Entity entity, string tileName)
+			:base(entity)
 		{
+			_componentType = ComponentType.TILE;
+
+			_tileDetails = StaticData.GetMapTileDetails(EnumConverter.ConvertStringToTileType(tileName));
+		}
+
+		public MapTileDetails MapTileDetails
+		{
+			get { return _tileDetails; }
 		}
 	}
 }
