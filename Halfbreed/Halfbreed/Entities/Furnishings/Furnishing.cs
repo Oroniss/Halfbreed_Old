d21870699ed0dc51b8ebe3e480789dcc0eb4c491
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-11 14:13:02 +1100

    Now an alternative contsructor to the Entity class.

--- Halfbreed/Halfbreed/Entities/Furnishings/Furnishing.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/Furnishing.cs
@@ -3,72 +3,31 @@ using Halfbreed.Entities;
 
 namespace Halfbreed
 {
-	public class Furnishing:Entity
+	public partial class Entity
 	{
 
-		private static int _currentMaxFurnishingId = 0;
-		private static List<int> _freeFurnishingIds = new List<int>();
-
-		private int _furnishingId;
-
-		protected Materials _material;
-
-		protected bool _hasMapTile;
-		protected MapTileDetails _mapTile;
-
-		public Furnishing(string furnishingName, Materials material, FurnishingTemplate template, int xLoc, int yLoc, string[] otherParameters)
-			:base(furnishingName, xLoc, yLoc)
+		public Entity(string furnishingName, Materials material, FurnishingTemplate template, int xLoc, int yLoc, string[] otherParameters)
+			
 		{
-			if (_freeFurnishingIds.Count == 0)
-			{
-				_furnishingId = _currentMaxFurnishingId;
-				_currentMaxFurnishingId += 1;
-			}
-			else
-			{
-				_furnishingId = _freeFurnishingIds[0];
-				_freeFurnishingIds.RemoveAt(0);
-			}
-
-			_material = material;
-			_displayLayer = DisplayLayer.FURNISHING;
+			_entityId = GetNextId();
+			_entityName = furnishingName;
+			_xLoc = xLoc;
+			_yLoc = yLoc;
+			_components = new Dictionary<ComponentType, Component>();
 
 			MaterialProperties properties = StaticData.GetProperties(material);
 
+			_displayLayer = DisplayLayer.FURNISHING;
 			_fgColor = properties.FGColor;
 			_symbol = template.Symbol;
+
+			_components[ComponentType.MATERIAL] = new MaterialComponent(this, material);
+
+			if (template.HasTile)
+				_components[ComponentType.TILE] = new TileComponent(this, template.TileTypeName);
+
 		}
 
-		public Materials Material
-		{
-			get { return _material; }
-		}
-
-		public override string GetDescription()
-		{
-			// TODO: Need to actually implement this.
-			return EntityName;
-		}
-
-		public bool HasMapTile
-		{
-			get { return _hasMapTile; }
-		}
-
-		public MapTileDetails MapTile
-		{
-			get {
-				if (_hasMapTile)
-				{
-					return _mapTile;
-				}
-				else
-				{
-					ErrorLogger.AddDebugText("Tried to access non-existant map tile on " + this.ToString());
-					return StaticData.GetMapTileDetails(TileType.WOODFLOOR);
-				}
-			}
-		}
 
 	}
 
