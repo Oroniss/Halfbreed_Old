b95977a71e3ef7593a3e34bcc8b2848909a1c2d0
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-11 14:11:55 +1100

    Items and furnishings - just a storage place for the material.

--- Halfbreed/Halfbreed/Entities/Components/MaterialComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/MaterialComponent.cs
@@ -1,10 +1,18 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
-	public class MaterialComponent
+	public class MaterialComponent:Component
 	{
-		public MaterialComponent()
+		private Materials _material;
+
+		public MaterialComponent(Entity entity, Materials material)
+			:base(entity)
 		{
+			_material = material;
+		}
+
+		public Materials Material
+		{
+			get { return _material; }
 		}
 	}
 }
