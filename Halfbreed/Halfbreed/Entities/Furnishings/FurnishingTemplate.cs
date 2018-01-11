5f6291296011104431835d2ad396417c86679cab
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-11 14:13:33 +1100

    Added tile information.

--- Halfbreed/Halfbreed/Entities/Furnishings/FurnishingTemplate.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/FurnishingTemplate.cs
@@ -5,12 +5,16 @@
 		private string _furnishingName;
 		private char _symbol;
 		private int _volume;
+		private bool _hasTile;
+		private string _tileTypeName;
 
-		public FurnishingTemplate(string furnishingName, char symbol, int volume)
+		public FurnishingTemplate(string furnishingName, char symbol, int volume, bool hasTile, string tileTypeName)
 		{
 			_furnishingName = furnishingName;
 			_symbol = symbol;
 			_volume = volume;
+			_hasTile = hasTile;
+			_tileTypeName = tileTypeName;
 		}
 
 		public string FurnishingName
@@ -26,6 +30,16 @@
 		public int Volume
 		{
 			get { return _volume; }
+		}
+
+		public bool HasTile
+		{
+			get { return _hasTile; }
+		}
+
+		public string TileTypeName
+		{
+			get { return _tileTypeName; }
 		}
 	}
 }
