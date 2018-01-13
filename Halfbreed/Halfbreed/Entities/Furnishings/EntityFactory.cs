a55959e899f529071f790a3c7683a5a7589b86ad
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-13 11:35:36 +1100

    Added in traits.

--- Halfbreed/Halfbreed/Entities/Furnishings/EntityFactory.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/EntityFactory.cs
@@ -2,17 +2,19 @@
 
 namespace Halfbreed.Entities
 {
-	public static class FurnishingFactory
+	public static class EntityFactory
 	{
-
 		private static Dictionary<string, FurnishingTemplate> _furnishings = new Dictionary<string, FurnishingTemplate>();
+
+
 
 		public static Entity CreateFurnishing(string furnishingName, Materials material, int xLoc, int yLoc, string[] otherParams)
 		{
 			if (!_furnishings.ContainsKey(furnishingName))
 				_furnishings[furnishingName] = EntityDatabaseConnection.GetFurnishingDetails(furnishingName);
 
-			Entity newFurnishing = new Entity(furnishingName, material, _furnishings[furnishingName], xLoc, yLoc, otherParams);
+			Entity newFurnishing = new Entity(furnishingName, material, _furnishings[furnishingName],
+											  xLoc, yLoc, otherParams);
 			return newFurnishing;
 		}
 	}
