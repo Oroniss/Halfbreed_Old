62908a6744257e21511681b953c816f64b185b8e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-13 11:37:33 +1100

    Another converter - easy enough.

--- Halfbreed/Halfbreed/NameConverters/StringToTraitConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/StringToTraitConverter.cs
@@ -1,10 +1,39 @@
-﻿using System;
-namespace Halfbreed
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Converters
 {
 	public class StringToTraitConverter
 	{
-		public StringToTraitConverter()
+		private static Dictionary<string, EntityTraits> _stringToTraits = new Dictionary<string, EntityTraits>()
 		{
+			{"Furnishing", EntityTraits.FURNISHING},
+			{"Ruined", EntityTraits.RUINED},
+			{"Elevation Change", EntityTraits.ELEVATIONCHANGE},
+			{"Immune to Fire", EntityTraits.IMMUNETOFIRE},
+			{"Immune to Cold", EntityTraits.IMMUNETOCOLD},
+			{"Immune to Acid", EntityTraits.IMMUNETOACID},
+			{"Immune to Electricity", EntityTraits.IMMUNETOELEC},
+			{"Immune to Mental", EntityTraits.IMMUNETOMENTAL},
+			{"Immune to Poison", EntityTraits.IMMUNETOPOISON},
+			{"Immune to Disease", EntityTraits.IMMUNETODISEASE},
+			{"Wood", EntityTraits.WOOD},
+			{"Organic", EntityTraits.ORGANIC},
+			{"Metal", EntityTraits.METAL},
+			{"Inorganic", EntityTraits.INORGANIC},
+			{"Stone", EntityTraits.STONE},
+			{"Gem", EntityTraits.GEM},
+			{"Leather", EntityTraits.LEATHER},
+			{"Cloth", EntityTraits.CLOTH}
+		};
+
+		public EntityTraits ConvertStringToTrait(string traitName)
+		{
+			return _stringToTraits[traitName];
+		}
+
+		public static int GetNumberOfTraits()
+		{
+			return _stringToTraits.Count;
 		}
 	}
 }
