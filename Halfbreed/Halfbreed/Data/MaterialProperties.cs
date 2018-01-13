6a8601c7116230d972e96cff4e40063aba430188
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-13 11:34:25 +1100

    Added traits and an adjective attribute.

--- Halfbreed/Halfbreed/Data/MaterialProperties.cs
+++ Halfbreed/Halfbreed/Data/MaterialProperties.cs
@@ -7,10 +7,12 @@
 		private int _weightPerUnitVolume;
 		private int _hardness;
 		private Colors _fgColor;
+		private EntityTraits[] _traits;
+		private string _adjective;
 
 		public MaterialProperties(int acid, int cold, int electricity, int fire, int poison, int disease, int light,
 								  int shadow, int mental, int physical, int nether, int hpPV, int weightPV, 
-		                          int hardness, Colors fgColor)
+		                          int hardness, Colors fgColor, EntityTraits[] traits, string adjective)
 		{
 			_defensiveStatTemplate = new Entities.EntityDefensiveStatTemplate(acid, cold, electricity, fire, poison,
 																			  disease, light, shadow, mental, physical, nether);
@@ -19,6 +21,8 @@
 			_weightPerUnitVolume = weightPV;
 			_hardness = hardness;
 			_fgColor = fgColor;
+			_traits = traits;
+			_adjective = adjective;
 		}
 
 		public int HPPerUnitVolume
@@ -36,6 +40,16 @@
 		public Colors FGColor
 		{
 			get { return _fgColor; }
+		}
+
+		public EntityTraits[] Traits
+		{
+			get { return _traits;}
+		}
+
+		public string Adjective
+		{
+			get { return _adjective; }
 		}
 
 		public Entities.EntityDefensiveStatTemplate DefensiveStatTemplate
