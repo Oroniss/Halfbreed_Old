232e19441b4fcba45851241d3503b67587f0f3f7
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-15 13:10:47 +1100

    Struct to store things coming out of DB for harvesting nodes - needs to add the loot table stuff at some point.

--- Halfbreed/Halfbreed/Entities/Furnishings/HarvestingTemplate.cs
+++ Halfbreed/Halfbreed/Entities/Furnishings/HarvestingTemplate.cs
@@ -1,7 +1,31 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed
 {
 	public struct HarvestingTemplate
 	{
+		private string _nodeName;
+		private HarvestingNodeType _nodeType;
+		private object _lootLists; // TODO: Figure out the best way to store and parse these.
+
+		public HarvestingTemplate(string nodeName, HarvestingNodeType nodeType, object lootLists)
+		{
+			_nodeName = nodeName;
+			_nodeType = nodeType;
+			_lootLists = lootLists;
+		}
+
+		public string NodeName
+		{
+			get { return _nodeName; }
+		}
+
+		public HarvestingNodeType NodeType
+		{
+			get { return _nodeType; }
+		}
+
+		public object LootLists
+		{
+			get { return _lootLists; }
+		}
 	}
 }
