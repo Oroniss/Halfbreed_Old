e13c742e20400d0fd7f5062556d1b6d58a1f3b4b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-15 15:03:49 +1100

    Put into own namespace and tidied up a bit. Added tests for Parsing the tile dictionary. Also renamed to test static data instead of "Entities"

--- Halfbreed/Halfbreed/Tests/StaticDataTests.cs
+++ Halfbreed/Halfbreed/Tests/StaticDataTests.cs
@@ -1,12 +1,11 @@
 using System;
-using Halfbreed.Entities;
 
 using NUnit.Framework;
 
 namespace Halfbreed.Tests
 {
 	[TestFixture]
-	public class EntityTests
+	public class StaticDataTests
 	{
 
 		[SetUp]
@@ -26,6 +25,19 @@ namespace Halfbreed.Tests
 			Assert.AreEqual(400, StaticData.GetProperties(Materials.HESSION).DefensiveStatTemplate.LightResistance);
 			Assert.AreEqual(10, StaticData.GetProperties(Materials.TIN).Hardness);
 			Assert.AreEqual(Colors.TAN, StaticData.GetProperties(Materials.HESSION).FGColor);
+		}
+
+		public void TestMapTileDictionary()
+		{
+			Assert.AreEqual(Enum.GetValues(typeof(Levels.TileType)).Length, StaticData.GetNumberOfTiles());
+
+			Assert.AreEqual(Colors.DARKWOODBROWN, StaticData.GetMapTileDetails(Levels.TileType.WOODWALL).BGColor);
+			Assert.AreEqual(Colors.DARKBROWN, StaticData.GetMapTileDetails(Levels.TileType.WOODWALL).FogColor);
+			Assert.AreEqual(20, StaticData.GetMapTileDetails(Levels.TileType.WOODENDEBRIS).Elevation);
+			Assert.AreEqual(3, StaticData.GetMapTileDetails(Levels.TileType.PALLET).Elevation);
+			Assert.AreEqual(0, StaticData.GetMapTileDetails(Levels.TileType.PLATFORM).MoveModes);
+			Assert.IsTrue(StaticData.GetMapTileDetails(Levels.TileType.WOODFLOOR).AllowLOS);
+			Assert.IsFalse(StaticData.GetMapTileDetails(Levels.TileType.WOODWALL).AllowLOS);
 		}
 
 		[TearDown]
