8fb350e1f73196621972d820c597609d9339710b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-19 10:45:49 +1100

    Added a player constructor for Entity class - will likely change a lot.

--- Halfbreed/Halfbreed/Entities/Player/Player.cs
+++ Halfbreed/Halfbreed/Entities/Player/Player.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Halfbreed.Entities
 {
 	public partial class Entity
 	{
-		public Entity()
+		public Entity(Menus.NewGameParameters playerParameters)
 		{
-			
+			_entityId = GetNextId();
+
+			_displayLayer = DisplayLayer.PLAYER;
+			_symbol = '@';
+			_fgColor = Colors.BLACK;
+			_entityName = "Player"; // TODO: Think whether this is correct or not.
+
+			_components = new Dictionary<ComponentType, Component>();
+			_traits = new List<EntityTraits>();
+			AddTrait(EntityTraits.PLAYER);
+			AddTrait(EntityTraits.BLOCKMOVE);
+
+			InputComponent inputComponent = new InputComponent(this);
+			inputComponent.SetManual();
+			_components[ComponentType.INPUT] = inputComponent;
+			_components[ComponentType.MOVEMENT] = new MovementComponent(this, new MovementModes[] { MovementModes.WALK });
 		}
 	}
 }
