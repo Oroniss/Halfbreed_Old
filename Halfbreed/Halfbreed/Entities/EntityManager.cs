e7733428c15ecccc860e698ea58d17dc1eae5550
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-19 10:45:12 +1100

    Added - then removed and merged into level - still not sure it's the best place for it though.

--- Halfbreed/Halfbreed/Entities/EntityManager.cs
+++ Halfbreed/Halfbreed/Entities/EntityManager.cs
@@ -1,31 +1,11 @@
 ﻿using System.Collections.Generic;
 namespace Halfbreed.Entities
 {
-	public class EntityRegister
+	public class EntityManager
 	{
+
 		private List<Entity> _currentEntities = new List<Entity>();
 		private List<Entity> _toDestroy = new List<Entity>();
-
-		public void RegisterEntity(Entity entity)
-		{
-			if (_currentEntities.Contains(entity))
-				ErrorLogger.AddDebugText("Tried to add existing entity to register: " + entity);
-			else
-				_currentEntities.Add(entity);
-		}
-
-		public void DeregisterEntity(Entity entity)
-		{
-			if (_currentEntities.Contains(entity))
-				_currentEntities.Remove(entity);
-			else
-				ErrorLogger.AddDebugText("Tried to deregister entity not present: " + entity);
-		}
-
-		public void FlagEntityForDestruction(Entity entity)
-		{
-			_toDestroy.Add(entity);
-		}
 
 		public void TakeTurn(int currentTime)
 		{
