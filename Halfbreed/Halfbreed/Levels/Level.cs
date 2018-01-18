575bda23cc2bba3a3fed32babd953563ccc75226
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-19 10:48:53 +1100

    Major refactoring - added the passibility pieces, also the entity manager pieces. Refactored the add/remove entity stuff a lot as well.

--- Halfbreed/Halfbreed/Levels/Level.cs
+++ Halfbreed/Halfbreed/Levels/Level.cs
@@ -19,7 +19,9 @@ namespace Halfbreed
 		private int _visibility;
 		private float _anathemaMultiplier;
 
-		private Dictionary<int, List<Entity>> _entities;
+		private List<Entity> _entities;
+		private List<Entity> _entitiesFlaggedForDestruction;
+		private Dictionary<int, List<Entity>> _entityLocations;
 		private Dictionary<int, MapTileDetails> _tilesWithTileEntities;
 
 		public Level(string LevelFilePath)
@@ -37,7 +39,9 @@ namespace Halfbreed
 			_visibility = Int32.Parse(LevelSpecificationFile.ReadLine());
 			_anathemaMultiplier = float.Parse(LevelSpecificationFile.ReadLine());
 
-			_entities = new Dictionary<int, List<Entity>>();
+			_entities = new List<Entity>();
+			_entitiesFlaggedForDestruction = new List<Entity>();
+			_entityLocations = new Dictionary<int, List<Entity>>();
 			_tilesWithTileEntities = new Dictionary<int, MapTileDetails>();
 
 			LevelSpecificationFile.ReadLine(); // Move to start of dictionary.
@@ -88,7 +92,7 @@ namespace Halfbreed
 					otherParams[i] = splitLine[i + FURNISHINGPREFIXLENGTH];
 				Entity newFurnishing = Entities.EntityFactory.CreateFurnishing(furnishingName, material,
 																					   xLoc, yLoc, otherParams);
-				AddEntity(xLoc, yLoc, newFurnishing);
+				AddEntity(newFurnishing);
 			}
 
 			while (true)
@@ -103,7 +107,7 @@ namespace Halfbreed
 				int yLoc = Int32.Parse(splitLine[2]);
 				Entity newHarvestingNode = Entities.EntityFactory.CreateHarvestingNode(harvestingName, xLoc, yLoc);
 
-				AddEntity(xLoc, yLoc, newHarvestingNode);
+				AddEntity(newHarvestingNode);
 			}
 
 			levelStream.Close();
@@ -159,77 +163,103 @@ namespace Halfbreed
 			_revealed[ConvertXYToInt(x, y)] = true;
 		}
 
-		public void AddEntity(int x, int y, Entity entity)
+		public void AddEntity(Entity entity)
 		{
-			int index = ConvertXYToInt(x, y);
+			int index = ConvertXYToInt(entity.XLoc, entity.YLoc);
 
-			if (entity.HasComponent(ComponentType.TILE))
-			{
-				if (_tilesWithTileEntities.ContainsKey(index))
-				{
-					ErrorLogger.AddDebugText(string.Format("Tried to add another tile component to same location." +
-														   "New entity = {0}, existing entity = {1}.",
-														   _tilesWithTileEntities[index], entity));
-					return;
-				}
-				_tilesWithTileEntities[index] = ((TileComponent)entity.GetComponent(ComponentType.TILE)).MapTileDetails;
-			}
-
-			if (_entities.ContainsKey(index))
-			{
-				_entities[index].Add(entity);
-				_entities[index].Sort();
-			}
-			else
-				_entities[index] = new List<Entity>() { entity };
-			
+			AddAtLocation(entity, index);
+			RegisterEntity(entity);
 		}
 
-		public void RemoveEntity(int x, int y, Entity entity)
+		public void RemoveEntity(Entity entity)
 		{
-			int index = ConvertXYToInt(x, y);
+			int index = ConvertXYToInt(entity.XLoc, entity.YLoc);
 
+			RemoveFromLocation(entity, index);
+			DeregisterEntity(entity);
+		}
+
+		private void AddAtLocation(Entity entity, int index)
+		{
 			if (entity.HasComponent(ComponentType.TILE))
+                AddTileEntity(entity, index);
+
+			if (_entityLocations.ContainsKey(index))
 			{
-				if (_tilesWithTileEntities.ContainsKey(index) && 
-				    _tilesWithTileEntities[index] == ((TileComponent)entity.GetComponent(ComponentType.TILE)).MapTileDetails)
-					_tilesWithTileEntities.Remove(index);
-				else
-					ErrorLogger.AddDebugText(string.Format("Tried to remove a tile entity but not present in db" +
-														   "Entity = {0}, x = {1}, y = {2}", entity, x, y));
+				_entityLocations[index].Add(entity);
+				_entityLocations[index].Sort();
 			}
+			else
+				_entityLocations[index] = new List<Entity>() { entity };
+		}
 
-			if (_entities.ContainsKey(index))
+		private void RemoveFromLocation(Entity entity, int index)
+		{
+			if (entity.HasComponent(ComponentType.TILE))
+                RemoveTileEntity(entity, index);
+
+			if (_entityLocations[index].Count == 1)
+				_entityLocations.Remove(index);
+			else
 			{
-				if (_entities[index].Contains(entity))
-				{
-					if (_entities[index].Count == 1)
-						_entities.Remove(index);
-					else
-					{
-						_entities[index].Remove(entity);
-						_entities[index].Sort();
-					}
-					return;
-				}
+				_entityLocations[index].Remove(entity);
+				_entityLocations[index].Sort();
 			}
-			ErrorLogger.AddDebugText(String.Format("Tried to remove non-existant Entity: {0} at index {1}", entity, index)); 
+		}
 
+		private void AddTileEntity(Entity entity, int index)
+		{
+			_tilesWithTileEntities[index] = ((TileComponent)entity.GetComponent(ComponentType.TILE)).MapTileDetails;
+		}
 
+		private void RemoveTileEntity(Entity entity, int index)
+		{
+			_tilesWithTileEntities.Remove(index);
+		}
+
+		private void RegisterEntity(Entity entity)
+		{
+			_entities.Add(entity);
+		}
+
+		private void DeregisterEntity(Entity entity)
+		{
+			_entities.Remove(entity);
+		}
+
+		public bool MoveEntityAttempt(Entity entity, int newX, int newY)
+		{
+			// TODO: Do the checks for movement here.
+			// Try to leave.
+			// Leave
+			// Try to enter
+			MoveEntity(entity, newX, newY);
+			// TODO: Enter
+			return true;
+		}
+
+		public void MoveEntity(Entity entity, int newX, int newY)
+		{
+			int oldIndex = ConvertXYToInt(entity.XLoc, entity.YLoc);
+			int newIndex = ConvertXYToInt(newX, newY);
+
+			RemoveFromLocation(entity, oldIndex);
+			entity.UpdatePosition(newX, newY);
+			AddAtLocation(entity, newIndex);
 		}
 
 		public bool HasEntity(int x, int y)
 		{
-			return _entities.ContainsKey(ConvertXYToInt(x, y));
+			return _entityLocations.ContainsKey(ConvertXYToInt(x, y));
 		}
 
 		public List<Entity> GetEntities(int x, int y)
 		{
 			List<Entity> returnList = new List<Entity>();
 
-			if (_entities.ContainsKey(ConvertXYToInt(x, y)))
+			if (_entityLocations.ContainsKey(ConvertXYToInt(x, y)))
 			{
-				foreach (Entity entity in _entities[ConvertXYToInt(x, y)])
+				foreach (Entity entity in _entityLocations[ConvertXYToInt(x, y)])
 					returnList.Add(entity);
 			}
 
@@ -239,7 +269,7 @@ namespace Halfbreed
 		public bool HasDrawingEntity(int x, int y)
 		{
 			int index = ConvertXYToInt(x, y);
-			return _entities.ContainsKey(index) && _entities[index][0].DisplayLayer != DisplayLayer.NOTDISPLAYED;
+			return _entityLocations.ContainsKey(index) && _entityLocations[index][0].DisplayLayer != DisplayLayer.NOTDISPLAYED;
 		}
 
 		public Entity GetDrawingEntity(int x, int y)
@@ -247,7 +277,61 @@ namespace Halfbreed
 			// TODO: Keep an eye on this to see if it needs to be added back.
 			// Error check removed since it should only be called from one place.
 			// if(_entities.ContainsKey(ConvertXYToInt(x, y)))
-			return _entities[ConvertXYToInt(x, y)][0];
+			return _entityLocations[ConvertXYToInt(x, y)][0];
 		}
+
+		private MapTileDetails GetTileDetails(int index)
+		{
+			if (_tilesWithTileEntities.ContainsKey(index))
+				return _tilesWithTileEntities[index];
+			else
+				return _mapGrid[index];
+		}
+
+		public void ActivateEntities(int currentTime)
+		{
+			for (int i = 0; i < _entities.Count; i++)
+				_entities[i].Update(currentTime);
+
+			while (_entitiesFlaggedForDestruction.Count > 0)
+			{
+				Entity toDestroy = _entitiesFlaggedForDestruction[0];
+				// TODO: Call the destroy method.
+				_entitiesFlaggedForDestruction.RemoveAt(0);
+			}
+		}
+
+		public bool IsPassible(int x, int y, MovementModes movementMode)
+		{
+			if (!IsValidMapCoord(x, y))
+				return false;
+
+			int index = ConvertXYToInt(x, y);
+
+			EntityTraits blockingTrait = EntityTraits.BLOCKMOVE;
+			if (movementMode == MovementModes.PHASE)
+				blockingTrait = EntityTraits.BLOCKPHASING;
+			if (IsBlockedByEntity(index, blockingTrait))
+				return false;
+
+			return movementMode > GetTileDetails(index).MoveModes;
+		}
+
+		private int GetElevation(int index)
+		{
+			return GetTileDetails(index).Elevation;
+		}
+
+		private bool IsBlockedByEntity(int index, EntityTraits blockingTrait)
+		{
+			if (_entityLocations.ContainsKey(index))
+				foreach (Entity entity in _entityLocations[index])
+				{
+					if (entity.HasTrait(blockingTrait))
+						return true;
+				}
+			return false;
+		}
+
 	}
 }
