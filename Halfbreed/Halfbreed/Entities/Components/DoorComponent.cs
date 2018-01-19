0ced56d00c55ce7a4f483633ee2fe7680273538e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-20 00:05:03 +1100

    Added door component, including setup. UseDoor still needs testing though.

--- Halfbreed/Halfbreed/Entities/Components/DoorComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/DoorComponent.cs
@@ -1,10 +1,56 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class EmptyClass
+	public class DoorComponent:Component
 	{
-		public EmptyClass()
+		private bool _isOpen;
+		private bool _isLocked;
+
+		public DoorComponent(Entity entity, bool isOpen)
+			:base(entity)
 		{
+			_componentType = ComponentType.DOOR;
+			_isLocked = false;
+			_isOpen = isOpen;
+
+			((InteractibleComponent)_entity.GetComponent(ComponentType.INTERACTIBLE)).AddFunction("UseDoor");
+
+			if (!isOpen)
+			{
+				_entity.AddTrait(EntityTraits.BLOCKMOVE);
+				_entity.Symbol = '+';
+			}
+		}
+
+		public DoorComponent(Entity entity, bool isLocked, object key)
+			: this(entity, false)
+		{
+			_isLocked = true;
+			// TODO: Implement the locked stuff here.
+		}
+
+		public bool IsOpen
+		{ 
+			get { return _isOpen; }
+		}
+
+		public void Use(Entity actor, int currentTime)
+		{
+			if (_isLocked)
+				// TODO: Figure this out.
+				return;
+
+			if (_isOpen)
+			{
+				_isOpen = false;
+				_entity.Symbol = '+';
+				_entity.AddTrait(EntityTraits.BLOCKMOVE);
+			}
+			else
+			{
+				_isOpen = true;
+				_entity.Symbol = '-';
+				_entity.RemoveTrait(EntityTraits.BLOCKMOVE);
+			}
 		}
 	}
 }
