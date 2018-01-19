d9bdb9e59bcb22c95bc18ad9cf04ffbff8f302e9
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-20 00:04:01 +1100

    Fixed parameter ordering bug.

--- Halfbreed/Halfbreed/Display/MenuConsole.cs
+++ Halfbreed/Halfbreed/Display/MenuConsole.cs
@@ -5,8 +5,8 @@ namespace Halfbreed.Display
 {
 	public class MenuConsole : BaseConsole
 	{
-		public MenuConsole(int width, int height, int top, int left, RLColor backColor, BackConsole backConsole)
-			: base(width, height, top, left, backColor, backConsole)
+		public MenuConsole(int width, int height, int left, int top, RLColor backColor, BackConsole backConsole)
+			: base(width, height, left, top, backColor, backConsole)
 		{
 		}
 
