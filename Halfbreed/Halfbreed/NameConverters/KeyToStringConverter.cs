f945ce8c37f55c8783884dada4c6b2883d726265
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-20 00:11:35 +1100

    Added the Letter keys to the converter.

--- Halfbreed/Halfbreed/NameConverters/KeyToStringConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/KeyToStringConverter.cs
@@ -9,11 +9,12 @@ namespace Halfbreed.Converters
 		// TODO: Add other keys as we go along.
         private Dictionary<RLKey, String> keyToString = 
         new Dictionary<RLKey, string>(){
-            {RLKey.Escape, "ESCAPE"},
-            {RLKey.Up, "UP"},
-            {RLKey.Down, "DOWN"},
-            {RLKey.Left, "LEFT"},
-            {RLKey.Right, "RIGHT"},
+			{RLKey.Escape, "ESCAPE"},
+			{RLKey.Up, "UP"},
+			{RLKey.Down, "DOWN"},
+			{RLKey.Left, "LEFT"},
+			{RLKey.Right, "RIGHT"},
+
 			{RLKey.Number0, "0"},
 			{RLKey.Number1, "1"},
 			{RLKey.Number2, "2"},
@@ -24,6 +25,33 @@ namespace Halfbreed.Converters
 			{RLKey.Number7, "7"},
 			{RLKey.Number8, "8"},
 			{RLKey.Number9, "9"},
+
+			{RLKey.A, "A"},
+			{RLKey.B, "B"},
+			{RLKey.C, "C"},
+			{RLKey.D, "D"},
+			{RLKey.E, "E"},
+			{RLKey.F, "F"},
+			{RLKey.G, "G"},
+			{RLKey.H, "H"},
+			{RLKey.I, "I"},
+			{RLKey.J, "J"},
+			{RLKey.K, "K"},
+			{RLKey.L, "L"},
+			{RLKey.M, "M"},
+			{RLKey.N, "N"},
+			{RLKey.O, "O"},
+			{RLKey.P, "P"},
+			{RLKey.Q, "Q"},
+			{RLKey.R, "R"},
+			{RLKey.S, "S"},
+			{RLKey.T, "T"},
+			{RLKey.U, "U"},
+			{RLKey.V, "V"},
+			{RLKey.W, "W"},
+			{RLKey.X, "X"},
+			{RLKey.Y, "Y"},
+			{RLKey.Z, "Z"}
         };
 
         public string convertKeyToString(RLKey key)
