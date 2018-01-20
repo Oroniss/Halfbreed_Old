3fa6b38ecb0c7e131d9d3bb612d3df929b12176c
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-20 21:28:20 +1100

    Essentially a vector class - identical to position but separate to make the distinction.

---
+++ Halfbreed/Halfbreed/Data/Direction.cs
@@ -0,0 +1,24 @@
+﻿namespace Halfbreed
+{
+	public class Direction
+	{
+		private int _xDirection;
+		private int _yDirection;
+
+		public Direction(int x, int y)
+		{
+			_xDirection = x;
+			_yDirection = y;
+		}
+
+		public int XDirection
+		{
+			get { return _xDirection; }
+		}
+
+		public int YDirection
+		{
+			get { return _yDirection; }
+		}
+	}
+}
