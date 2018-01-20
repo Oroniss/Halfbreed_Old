2c455c617521064bdb6d5d11b477015e286ccb19
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-20 21:31:25 +1100

    Removed RLKey converter.

--- Halfbreed/Halfbreed/NameConverters/EnumConverter.cs
+++ Halfbreed/Halfbreed/NameConverters/EnumConverter.cs
@@ -4,16 +4,10 @@ namespace Halfbreed
 {
 	public static class EnumConverter
 	{
-		private static KeyToStringConverter KeyToStringConverter = new KeyToStringConverter();
 		private static CharacterClassToStringConverter CharacterClassToStringConverter = new CharacterClassToStringConverter();
 
 		public static string ConvertEnumToString(object enumValue)
 		{
-			if(enumValue is RLNET.RLKey)
-			{
-				return KeyToStringConverter.convertKeyToString((RLNET.RLKey)enumValue);
-			}
-
 			if (enumValue is CharacterClasses)
 			{
 				return CharacterClassToStringConverter.ConvertCharacterClassToString(
