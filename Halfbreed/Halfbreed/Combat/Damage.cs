63aec9b7a9fb12570f487a82117ca2c4ed36a344
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-22 09:06:04 +1100

    Early stages - something to track damage through the game.

--- Halfbreed/Halfbreed/Combat/Damage.cs
+++ Halfbreed/Halfbreed/Combat/Damage.cs
@@ -1,10 +1,48 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Combat
 {
 	public class Damage
 	{
-		public Damage()
+		private DamageType _damageType;
+		private int _originalDamageAmount;
+		private int _damageModifiers;
+		private bool _negateDamage;
+
+		public Damage(DamageType damageType, int originalDamageAmount)
 		{
+			_damageType = damageType;
+			_originalDamageAmount = originalDamageAmount;
+			_damageModifiers = 0;
+			_negateDamage = false;
+		}
+
+		public void ModifyDamage(int amount)
+		{
+			_damageModifiers += amount;
+		}
+
+		public void NegateDamage()
+		{
+			_negateDamage = true;
+		}
+
+		public DamageType DamageType
+		{
+			get { return _damageType; }
+		}
+
+		public int OriginalDamageAmount
+		{
+			get { return _originalDamageAmount; }
+		}
+
+		public int FinalDamageAmount
+		{
+			get
+			{
+				if (_negateDamage)
+					return 0;
+				return System.Math.Max(0, _originalDamageAmount + _damageModifiers);
+			}
 		}
 	}
 }
