2a1e316c8317929ac82f739b8f97ee1f8e1bda47
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-22 09:02:05 +1100

    Component for something that happens when actually moving off - briarpatch, some triggers, etc.

--- Halfbreed/Halfbreed/Entities/Components/MoveOffComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/MoveOffComponent.cs
@@ -1,10 +1,20 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class MoveOffComponent
+	public partial class MoveOffComponent:MovementTriggerComponent
 	{
-		public MoveOffComponent()
+		public MoveOffComponent(Entity entity)
+			:base(entity)
 		{
+			_componentType = ComponentType.MOVEOFF;
 		}
+
+	public void MoveOff(Entity actor, int destinationX, int destinationY)
+	{
+		foreach (string functionName in _functions)
+				_moveOffFunctionDictionary[functionName](_entity, actor, destinationX, destinationY);
+	}
+
+	private delegate void MoveOnFunction(Entity furnishing, Entity actor, int destinationX, int destinationY);
+
 	}
 }
