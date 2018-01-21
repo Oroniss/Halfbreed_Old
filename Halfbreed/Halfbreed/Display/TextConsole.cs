30789d314f8d8ec5d725ad55ded4fd3585bed2bb
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-21 11:20:21 +1100

    Added wrap text function and tested the split text function.

--- Halfbreed/Halfbreed/Display/TextConsole.cs
+++ Halfbreed/Halfbreed/Display/TextConsole.cs
@@ -1,8 +1,9 @@
 ﻿using RLNET;
+using System.Collections.Generic;
 
 namespace Halfbreed.Display
 {
-	public class TextDisplay:BaseConsole
+	public class TextConsole:BaseConsole
 	{
 		private string[] _outputText;
 		private int _currentIndex;
@@ -11,9 +12,9 @@ namespace Halfbreed.Display
 		private int _textWidth;
 		private int _textYOffset = 3; // 1 Blank, 1 header, 1 more blank.
 		private int _textHeight;
-		private int _arrayLength = 100;
+		private int _arrayLength = 100; // TODO: Put this into a config file somewhere.
 
-		public TextDisplay(int width, int height, int left, int top, RLColor backColor, BackConsole backConsole)
+		public TextConsole(int width, int height, int left, int top, RLColor backColor, BackConsole backConsole)
 			:base(width, height, left, top, backColor, backConsole)
 		{
 			_textWidth = _console.Width - 2; // 1 on each side
@@ -36,22 +37,43 @@ namespace Halfbreed.Display
 				string[] splitText = text.Split('\n');
 				for (int i = splitText.Length - 1; i >= 0; i--)
 					AddOutputText(splitText[i]);
+				return;
 			}
 
 			if (text.Length > _textWidth)
 			{
-				string[] splitText = WrapText(text);
-				for (int i = splitText.Length - 1; i >= 0; i--)
+				List<string> splitText = WrapText(text);
+				for (int i = splitText.Count - 1; i >= 0; i--)
 					AddOutputText(splitText[i]);
+				return;
 			}
 
 			_outputText[_currentIndex] = text;
 			_currentIndex = (_currentIndex + 1) % _arrayLength;
+
+			DrawOutputText();
 		}
 
-		private string[] WrapText(string Text)
+		private List<string> WrapText(string Text)
 		{
-			return new string[] { };
+			List<string> lines = new List<string>();
+			while (true)
+			{
+				if (Text.Length <= _textWidth)
+				{
+					lines.Add(Text);
+					return lines;
+				}
+
+				int lastSpace = Text.Substring(0, _textWidth).LastIndexOf(' ');
+				if (lastSpace == -1)
+				{
+					ErrorLogger.AddDebugText("Message text too long without a space character.");
+					return lines;
+				}
+				lines.Add(Text.Substring(0, lastSpace));
+				Text = Text.Substring(lastSpace + 1);
+			}
 		}
 
 		private bool CheckIfLastTextIsEmpty()
@@ -63,7 +85,7 @@ namespace Halfbreed.Display
 		{
 			Clear();
 
-			_console.Print(_textXOffset, 1, "Message Log", Palette.GetColor(Colors.WHITE));
+			_console.Print(_textXOffset + (int)((_textWidth - 11) / 2), 1, "Message Log", Palette.GetColor(Colors.WHITE));
 
 			for (int i = 0; i < _textHeight; i++)
 			{
