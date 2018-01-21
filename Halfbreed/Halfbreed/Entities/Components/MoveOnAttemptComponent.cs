7beab4934ccc3acae78386603265b93be725ec49
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-22 09:02:39 +1100

    Component for trying to move on - could be climbing up somewhere, forcing through something, etc.

--- Halfbreed/Halfbreed/Entities/Components/MoveOnAttemptComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/MoveOnAttemptComponent.cs
@@ -1,10 +1,24 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class MoveOnAttemptComponent
+	public partial class MoveOnAttemptComponent:MovementTriggerComponent
 	{
-		public MoveOnAttemptComponent()
+		public MoveOnAttemptComponent(Entity entity)
+			:base(entity)
 		{
+			_componentType = ComponentType.MOVEONATTEMPT;
 		}
+
+		public bool MoveOnAttempt(Entity actor, int originX, int originY)
+		{
+			bool success = true;
+			foreach (string functionName in _functions)
+			{
+				if (!_moveOnAttemptFunctionDictionary[functionName](_entity, actor, originX, originY))
+					success = false;
+			}
+			return success;
+		}
+
+		private delegate bool MoveOnAttemptFunction(Entity entity, Entity actor, int originX, int originY);
 	}
 }
