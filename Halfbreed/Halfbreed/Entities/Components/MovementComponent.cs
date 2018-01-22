583bffc3f76c95a8fa7bd296a27f3a8d55329f71
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-22 14:03:04 +1100

    Gutted the class - only has an attempt move function now - movement modes have all become traits.

--- Halfbreed/Halfbreed/Entities/Components/MovementComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/MovementComponent.cs
@@ -1,66 +1,21 @@
-﻿using System.Collections.Generic;
-
-namespace Halfbreed.Entities
+﻿namespace Halfbreed.Entities
 {
 	public class MovementComponent:Component
 	{
-		List<MovementModes> _movementModes;
-		MovementModes _currentMovementMode;
-
-		public MovementComponent(Entity entity, MovementModes[] movementModes)
+		public MovementComponent(Entity entity)
 			:base(entity)
 		{
-			_movementModes = new List<MovementModes>();
-			foreach (MovementModes movementMode in movementModes)
-				AddMovementMode(movementMode);
-			_componentType = ComponentType.MOVEMENT;
-
-			//TODO: Need to add swimming stuff here too.
-		}
-
-		public MovementModes MovementMode
-		{
-			get { return _currentMovementMode; }
-		}
-
-		public void AddMovementMode(MovementModes mode)
-		{
-			_movementModes.Add(mode);
-			if (mode > _currentMovementMode)
-				_currentMovementMode = mode;
-		}
-
-		public void RemoveMovementMode(MovementModes mode)
-		{
-			_movementModes.Remove(mode);
-			if (mode == _currentMovementMode)
-				RecalculateCurrentMode();
-		}
-
-		public bool HasMovementMode(MovementModes mode)
-		{
-			return _movementModes.Contains(mode);
-		}
-
-		private void RecalculateCurrentMode()
-		{
-			_currentMovementMode = MovementModes.IMMOBILE;
-			foreach (MovementModes mode in _movementModes)
-			{
-				if (mode > _currentMovementMode)
-					_currentMovementMode = mode;
-			}
 		}
 
 		public bool AttemptMove(int newX, int newY)
 		{
 			if (_entity.HasTrait(EntityTraits.IMMOBILISED))
 			   return false;
-			
-			//TODO: Check for swimming here.
-			if (GameEngine.CurrentLevel.IsPassible(newX, newY, MovementMode))
+
+			if (GameEngine.CurrentLevel.IsPassible(newX, newY, _entity.HasTrait(EntityTraits.CANWALK),
+			                                       _entity.HasTrait(EntityTraits.CANFLY), _entity.HasTrait(EntityTraits.CANSWIM)))
 			{
-				GameEngine.CurrentLevel.MoveEntityAttempt(_entity, newX, newY);
+				GameEngine.CurrentLevel.MoveEntityAttempt(_entity, _entity.XLoc, _entity.YLoc, newX, newY);
 				return true;
 			}
 			return false;
