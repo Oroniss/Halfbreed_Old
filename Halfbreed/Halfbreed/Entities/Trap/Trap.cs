ebf242b1178135e31c374c054e3f7204349d16ff
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-23 10:46:13 +1100

    New Entity Constructor for traps.

--- Halfbreed/Halfbreed/Entities/Trap/Trap.cs
+++ Halfbreed/Halfbreed/Entities/Trap/Trap.cs
@@ -1,10 +1,24 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class Trap
+	public partial class Entity
 	{
-		public Trap()
+		public Entity(string trapName, int xLoc, int yLoc, int trapLevel, string[] otherParameters)
+			: this(trapName, xLoc, yLoc, new EntityTraits[] { })
 		{
+			_displayLayer = DisplayLayer.TRAP;
+			_symbol = '^';
+			_fgColor = Colors.RED;
+
+			AddTrait(EntityTraits.TRAP);
+			AddTrait(EntityTraits.FURNISHING);
+			foreach (EntityTraits trait in _furnishingTraits)
+				AddTrait(trait);
+
+			_components[ComponentType.TRAP] = new TrapComponent(this, trapName, trapLevel);
+
+			MoveOnComponent moveOn = new MoveOnComponent(this);
+			moveOn.AddFunction("TriggerTrap");
+			_components[ComponentType.MOVEON] = moveOn;
 		}
 	}
 }
