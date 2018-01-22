ae076d9fbf030fa478bf8be203facbc36edf8120
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-23 10:46:46 +1100

    Added some trap functions and the dictionary.

--- Halfbreed/Halfbreed/Entities/Trap/TrapFunctions.cs
+++ Halfbreed/Halfbreed/Entities/Trap/TrapFunctions.cs
@@ -1,10 +1,30 @@
-﻿using System;
-namespace Halfbreed
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Entities
 {
-	public class TrapFunctions
+	public partial class TrapComponent
 	{
-		public TrapFunctions()
+		private static Dictionary<string, TrapFunction> _trapFunctionDictionary = new Dictionary<string, TrapFunction>()
 		{
+			{"Swinging Blade", new TrapFunction(SwingingBladeTrap)},
+			{"Flame Vent Trap", new TrapFunction(FlameVentTrap)},
+			{"Pit Trap", new TrapFunction(PitTrap)}
+		};
+
+		private static void SwingingBladeTrap(TrapComponent trap, Entity actor)
+		{
+			actor.ProcessDamage(trap.Entity, new Combat.Damage(Combat.DamageType.PHYSICAL, 20 * trap.TrapLevel));
+			trap.DisarmTrap(actor);
+		}
+
+		private static void FlameVentTrap(TrapComponent trap, Entity actor)
+		{
+			actor.ProcessDamage(trap.Entity, new Combat.Damage(Combat.DamageType.FIRE, 30 * trap.TrapLevel));
+		}
+
+		private static void PitTrap(TrapComponent trap, Entity actor)
+		{
+			actor.ProcessDamage(trap.Entity, new Combat.Damage(Combat.DamageType.PHYSICAL, 10 * trap.TrapLevel));
 		}
 	}
 }
