fce4fcd530f73ceb28ace320cf66992a8d13447b
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-23 10:42:57 +1100

    Created - present as both an interaction trigger and a movement trigger.

--- Halfbreed/Halfbreed/Entities/Components/TrapComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/TrapComponent.cs
@@ -1,10 +1,38 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class TrapComponent
+	public partial class TrapComponent:Component
 	{
-		public TrapComponent()
+		private string _trapType;
+		private int _trapLevel;
+		bool _armed;
+
+		public TrapComponent(Entity entity, string trapType, int trapLevel)
+			:base(entity)
 		{
+			_componentType = ComponentType.TRAP;
+			_trapType = trapType;
+			_trapLevel = trapLevel;
+			_armed = true;
 		}
+
+		public int TrapLevel
+		{
+			get { return _trapLevel; }
+		}
+
+		public void TriggerTrap(Entity actor)
+		{
+			if(_armed)
+				_trapFunctionDictionary[_trapType](this, actor);
+		}
+
+		public bool DisarmTrap(Entity actor)
+		{
+			// TODO: Flesh this out properly later.
+			_armed = false;
+			return true;
+		}
+
+		private delegate void TrapFunction(TrapComponent trapComponent, Entity actor);
 	}
 }
