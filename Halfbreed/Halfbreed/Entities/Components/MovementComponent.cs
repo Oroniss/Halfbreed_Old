1c1183f81069dccc13f713b565324a5cd61ebbb6
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-23 10:42:34 +1100

    Reworked to reflect new movement arrangements. Tries to fly, then walk, then swim.

--- Halfbreed/Halfbreed/Entities/Components/MovementComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/MovementComponent.cs
@@ -11,11 +11,19 @@
 		{
 			if (_entity.HasTrait(EntityTraits.IMMOBILISED))
 			   return false;
-
-			if (GameEngine.CurrentLevel.IsPassible(newX, newY, _entity.HasTrait(EntityTraits.CANWALK),
-			                                       _entity.HasTrait(EntityTraits.CANFLY), _entity.HasTrait(EntityTraits.CANSWIM)))
+			if (_entity.HasTrait(EntityTraits.CANFLY) && GameEngine.CurrentLevel.IsFlyable(newX, newY))
 			{
-				GameEngine.CurrentLevel.MoveEntityAttempt(_entity, _entity.XLoc, _entity.YLoc, newX, newY);
+				GameEngine.CurrentLevel.FlyAttempt(_entity, _entity.XLoc, _entity.YLoc, newX, newY);
+				return true;
+			}
+			if (_entity.HasTrait(EntityTraits.CANWALK) && GameEngine.CurrentLevel.IsWalkable(newX, newY))
+			{
+				GameEngine.CurrentLevel.WalkAttempt(_entity, _entity.XLoc, _entity.YLoc, newX, newY);
+				return true;
+			}
+			if (_entity.HasTrait(EntityTraits.CANSWIM) && GameEngine.CurrentLevel.IsSwimmable(newX, newY))
+			{
+				GameEngine.CurrentLevel.SwimAttempt(_entity, _entity.XLoc, _entity.YLoc, newX, newY);
 				return true;
 			}
 			return false;
