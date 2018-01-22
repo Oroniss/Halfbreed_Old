c77867bd3bd8c19c5e95312180119274ab4d08ef
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-23 10:45:31 +1100

    Moved the dictionary here and added TriggerTrap

--- Halfbreed/Halfbreed/Entities/Furnishing/InteractionFunctions.cs
+++ Halfbreed/Halfbreed/Entities/Furnishing/InteractionFunctions.cs
@@ -1,7 +1,17 @@
-﻿namespace Halfbreed.Entities
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Entities
 {
 	public partial class InteractibleComponent
 	{
+		private static Dictionary<string, InteractionFunction> _interactionFunctionDictionary = new
+			Dictionary<string, InteractionFunction>()
+		{
+			{"NoUse", new InteractionFunction(NoUse)},
+			{"UseDoor", new InteractionFunction(UseDoor)},
+			{"TriggerTrap", new InteractionFunction(TriggerTrap)}
+		};
+
 		private static void UseDoor(Entity interactible, Entity actor, int currentTime)
 		{
 			DoorComponent doorComponent = (DoorComponent)interactible.GetComponent(ComponentType.DOOR);
@@ -11,6 +21,12 @@
 		private static void NoUse(Entity interactible, Entity actor, int currentTime)
 		{
 			MainGraphicDisplay.TextConsole.AddOutputText("You can't do anything with that!");
+		}
+
+		private static void TriggerTrap(Entity interactible, Entity actor, int currentTime)
+		{
+			TrapComponent trapComponent = (TrapComponent)interactible.GetComponent(ComponentType.TRAP);
+			trapComponent.TriggerTrap(actor);
 		}
 
 	}
