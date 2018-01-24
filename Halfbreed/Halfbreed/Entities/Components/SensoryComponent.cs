c25a5c0d3b82f743da705fab05f14064e7bcf9b7
jeffrey <jeffrey.fisher@anu.edu.au>
2018-01-24 13:21:59 +1100

    Pretty self explanatory - currently only view distance, but will likely get some degree of spot/listen in there as well.

--- Halfbreed/Halfbreed/Entities/Components/SensoryComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/SensoryComponent.cs
@@ -1,10 +1,33 @@
 ﻿using System;
-namespace Halfbreed
+
+namespace Halfbreed.Entities
 {
-	public class SensoryComponent
+	public class SensoryComponent:Component
 	{
-		public SensoryComponent()
+		static readonly int MAXIMUMVIEWDISTANCE = 24;
+
+		int _viewDistanceBase;
+		int _viewDistanceModifiers;
+
+		public SensoryComponent(Entity entity, int viewDistance)
+			:base(entity)
 		{
+			_componentType = ComponentType.SENSORY;
+			_viewDistanceBase = viewDistance;
+			_viewDistanceModifiers = 0;
+		}
+
+		public int CurrentViewDistance
+		{
+			get {if (_entity.HasTrait(EntityTraits.BLIND) && !_entity.HasTrait(EntityTraits.BLINDSIGHT))
+					return 0;
+
+				return Math.Min(Math.Max(_viewDistanceBase + _viewDistanceModifiers, 0), MAXIMUMVIEWDISTANCE); }
+		}
+
+		public void AddViewDistanceModifier(int modifier)
+		{
+			_viewDistanceModifiers += modifier;
 		}
 	}
 }
