42ce384d5968fb3e97b731786f8c4e26067eadcd
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-03 13:33:50 +1100

    Class setup for the storage of levels - will work short to medium term, will need a better long term solution.

--- Halfbreed/Halfbreed/Levels/LevelDictionary.cs
+++ Halfbreed/Halfbreed/Levels/LevelDictionary.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Halfbreed.Levels;
+
 namespace Halfbreed
 {
-	public class LevelDictionary
+	public partial class Level
 	{
-		public LevelDictionary()
+		static string _baseFilePath = System.IO.Directory.GetCurrentDirectory();
+
+		static Dictionary<LevelEnum, string> _filePaths = new Dictionary<LevelEnum, string>()
 		{
+			{LevelEnum.TESTLEVEL1, "/LevelFiles/Testing/TestLevel1"},
+			{LevelEnum.TESTLEVEL2, "/LevelFiles/Testing/TestLevel2"}
+		};
+
+		string GetFilePath(LevelEnum level)
+		{
+			return _baseFilePath + _filePaths[level] + ".txt";
 		}
+
+		public static void SetToTestDirectory(string testFilePath)
+		{
+			_baseFilePath = testFilePath;
+		}
+
 	}
 }
