d550106ed53862dbe770bff3e4c871211e2cf564
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-03 13:38:53 +1100

    Handles the level transition.

--- Halfbreed/Halfbreed/Entities/Components/LevelTransitionComponent.cs
+++ Halfbreed/Halfbreed/Entities/Components/LevelTransitionComponent.cs
@@ -1,10 +1,25 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Entities
 {
-	public class LevelTransitionComponent
+	public class LevelTransitionComponent:Component
 	{
-		public LevelTransitionComponent()
+		Levels.LevelEnum _destinationLevel;
+		int _newXLoc;
+		int _newYLoc;
+
+		public LevelTransitionComponent(Entity entity, Levels.LevelEnum destinationLevel, int newXLoc, int newYLoc)
+			:base(entity)
 		{
+			_componentType = ComponentType.LEVELTRANSITION;
+			_destinationLevel = destinationLevel;
+			_newXLoc = newXLoc;
+			_newYLoc = newYLoc;
+		}
+
+		// TODO: Currently bool in case something needs to stop this from happening in the future.
+		public bool MoveLevel(Entity actor)
+		{
+			GameEngine.LevelTransition(_destinationLevel, _newXLoc, _newYLoc);
+			return true;
 		}
 	}
 }
