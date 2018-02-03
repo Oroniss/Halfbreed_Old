adcd2ebc3397a86c628695eb43568bcad15b61fe
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-03 13:36:44 +1100

    Created an Enum of the levels in the game.

--- Halfbreed/Halfbreed/LevelFiles/LevelEnum.cs
+++ Halfbreed/Halfbreed/LevelFiles/LevelEnum.cs
@@ -1,7 +1,15 @@
-﻿using System;
-namespace Halfbreed
+﻿namespace Halfbreed.Levels
 {
 	public enum LevelEnum
 	{
+		// Testing Levels
+		TESTLEVEL1,
+		TESTLEVEL2,
+
+		// Act 1, Chapter 1
+		OUTCASTSEND,
+		WALLMARKETSTORAGEFACILITY1,
+		WALLMARKETSTORAGEFACILITY2
+
 	}
 }
