fcf94c6c91d291d422f596e95e446018a2416f3e
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-10 14:35:00 +1100

    Struct for storing configuration parameters - ended up being relatively small - might actually be worth removing later on.

--- Halfbreed/Halfbreed/UserData/ConfigParameters.cs
+++ Halfbreed/Halfbreed/UserData/ConfigParameters.cs
@@ -1,7 +1,19 @@
 ﻿using System;
-namespace Halfbreed
+
+namespace Halfbreed.UserData
 {
+	[Serializable]
 	public struct ConfigParameters
 	{
+		public bool ExtraKeys;
+		public bool FullLogging;
+		public bool GMOptions;
+
+		public ConfigParameters(bool extraKeys, bool fullLogging, bool gmOptions)
+		{
+			ExtraKeys = extraKeys;
+			FullLogging = fullLogging;
+			GMOptions = gmOptions;
+		}
 	}
 }
