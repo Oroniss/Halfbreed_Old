6c24d480e235727ae9afb307ab02778f8bbb626d
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-10 14:37:34 +1100

    Completely reworked - now a base class, not "The" class. No longer has components. Individual aspects are kept on the derived classes as appropriate.

--- Halfbreed/Halfbreed/Entities/Entity.cs
+++ Halfbreed/Halfbreed/Entities/Entity.cs
@@ -3,111 +3,49 @@ using System.Collections.Generic;
 
 namespace Halfbreed.Entities
 {
-	public partial class Entity : IComparable
+	public abstract class Entity
 	{
-		private string _entityName;
+		string _entityName;
 
-		private Dictionary<ComponentType, Component> _components;
+		int _xLoc;
+		int _yLoc;
 
-		private int _xLoc;
-		private int _yLoc;
+		Colors _fgColor;
+		char _symbol;
 
-		private DisplayLayer _displayLayer;
-		private Colors _fgColor;
-		private char _symbol;
+		List<Traits> _traits;
 
-		private List<EntityTraits> _traits;
+		bool _isConcealed;
+		bool _playerSpotted;
+		protected bool _destroyed;
 
-		private bool _hasTile;
-		private Levels.MapTileDetails _maptile;
+		List<object> _effects;
+		object _defensiveStats;
 
-		private ConcealedComponent _concealedComponent;
-		private bool _isConcealed;
-		private bool _playerSpotted;
+		Dictionary<string, string> _otherAttributes;
 
-		private Entity(string entityName, int xLoc, int yLoc, EntityTraits[] traits)
+		protected Entity(string entityName, int xLoc, int yLoc, List<string> otherParameters)
 		{
 			_entityName = entityName;
 			_xLoc = xLoc;
 			_yLoc = yLoc;
-			_components = new Dictionary<ComponentType, Component>();
-			_traits = new List<EntityTraits>();
 
-			_hasTile = false;
-			_isConcealed = false;
-			_playerSpotted = true;
+			_traits = new List<Traits>();
 
-			foreach (EntityTraits trait in traits)
+			_effects = null;
+			_defensiveStats = null;
+			_otherAttributes = new Dictionary<string, string>();
+
+			_isConcealed = otherParameters.Contains("Concealed");
+			_playerSpotted = !_isConcealed;
+			_destroyed = false;
+
+			var basicDetails = EntityData.GetEntityDetails(entityName);
+			_symbol = basicDetails.Symbol;
+			_fgColor = basicDetails.FGColor;
+			foreach (Traits trait in basicDetails.Traits)
 				AddTrait(trait);
-		}
 
-		private void SetupOtherComponents(string[] otherComponents, string[] otherParameters)
-		{
-			var allParams = MergeOptionalParameters(otherComponents, otherParameters);
-
-			// Door component
-			if (Array.IndexOf(allParams, "AddDoorComponent") != -1)
-			{
-				bool locked = (Array.IndexOf(allParams, "Locked") != -1);
-				if (locked)
-					_components[ComponentType.DOOR] = new DoorComponent(this, true, allParams);
-				else
-				{
-					bool isOpen = (Array.IndexOf(allParams, "Open") != -1);
-					_components[ComponentType.DOOR] = new DoorComponent(this, isOpen);
-				}
-			}
-
-			// Trap component
-			if (Array.IndexOf(allParams, "AddTrapComponent") != -1)
-			{
-				string trapType = allParams[Array.IndexOf(allParams, "TrapType") + 1];
-				var difficulty = int.Parse(allParams[Array.IndexOf(allParams, "TrapLevel") + 1]);
-				_components[ComponentType.TRAP] = new TrapComponent(this, trapType, difficulty);
-				((InteractibleComponent)_components[ComponentType.INTERACTIBLE]).AddFunction("TriggerTrap");
-			}
-
-			// Light source component
-			if (Array.IndexOf(allParams, "AddLightSourceComponent") != -1)
-			{
-				string lightType = allParams[Array.IndexOf(allParams, "LightType") + 1];
-				int radius = int.Parse(allParams[Array.IndexOf(allParams, "Radius") + 1]);
-				int durationRemaining = int.Parse(allParams[Array.IndexOf(allParams, "DurationRemaining") + 1]);
-				bool permanent = (Array.IndexOf(allParams, "PermanentLight") != -1);
-				bool lit = (Array.IndexOf(allParams, "IsLit") != -1);
-
-				_components[ComponentType.LIGHTSOURCE] = new LightSourceComponent(this, lightType, radius, durationRemaining,
-																				  permanent, lit);
-			}
-
-			if (Array.IndexOf(allParams, "Concealed") != -1)
-			{
-				// TODO: Set this up properly
-				_playerSpotted = false;
-				_isConcealed = true;
-				_concealedComponent = new ConcealedComponent(this, 1, "", ' ', (int)DisplayLayer.NOTDISPLAYED, true, "WOODWALL");
-
-			}
-
-			if (Array.IndexOf(allParams, "LevelTransition") != -1)
-			{
-				AddTrait(EntityTraits.INDESTRUCTIBLE);
-				((InteractibleComponent)_components[ComponentType.INTERACTIBLE]).AddFunction("UseLevelTransition");
-				var destination = (Levels.LevelEnum)Enum.Parse(typeof(Levels.LevelEnum), allParams[Array.IndexOf(allParams, "DestinationLevel") + 1]);
-				var newXLoc = int.Parse(allParams[Array.IndexOf(allParams, "NewXLoc") + 1]);
-				var newYLoc = int.Parse(allParams[Array.IndexOf(allParams, "NewYLoc") + 1]);
-				_components[ComponentType.LEVELTRANSITION] = new LevelTransitionComponent(this, destination, newXLoc, newYLoc);
-			}
-		}
-
-		private string[] MergeOptionalParameters(string[] otherComponents, string[] otherParameters)
-		{
-			var returnArray = new string[otherComponents.Length + otherParameters.Length];
-			for (int i = 0; i < otherComponents.Length; i++)
-				returnArray[i] = otherComponents[i];
-			for (int i = 0; i < otherParameters.Length; i++)
-				returnArray[i + otherComponents.Length] = otherParameters[i];
-			return returnArray;
 		}
 
 		public string EntityName
@@ -115,29 +53,16 @@ namespace Halfbreed.Entities
 			get { return _entityName; }
 		}
 
-		public DisplayLayer DisplayLayer
+		public virtual Colors FGColor
 		{
-			get {
-				if (!_playerSpotted)
-					return _concealedComponent.ConcealedDisplayLayer;
-				return _displayLayer;}
-			set { _displayLayer = value; }
-		}
-
-		public Colors FGColor
-		{
-			get {
-				if (!_playerSpotted)
-					return _concealedComponent.ConcealedFGColor;
-				return _fgColor;}
-			set { _fgColor = value; }
+			get { return _fgColor; }
 		}
 
 		public char Symbol
 		{
 			get {
 				if (!_playerSpotted)
-					return _concealedComponent.ConcealedSymbol;
+					return ' ';
 				return _symbol; }
 			set { _symbol = value; }
 		}
@@ -151,26 +76,6 @@ namespace Halfbreed.Entities
 		{
 			get { return _playerSpotted; }
 			set { _playerSpotted = value; }
-		}
-
-		public bool HasTile
-		{
-			get { return _hasTile || (!_playerSpotted && _concealedComponent.HasConcealedTile); }
-		}
-
-		public Levels.MapTileDetails TileDetails
-		{
-			get { if (!_playerSpotted && _concealedComponent.HasConcealedTile)
-					return _concealedComponent.ConcealedMapTile;
-				if (_hasTile)
-					return _maptile;
-				return null;
-			}
-		}
-
-		public ConcealedComponent ConcealedComponent
-		{
-			get { return _concealedComponent; }
 		}
 
 		public override string ToString()
@@ -193,30 +98,12 @@ namespace Halfbreed.Entities
 			get { return _yLoc; }
 		}
 
-		public void UpdatePosition(Position newPosition)
-		{
-			_xLoc = newPosition.X;
-			_yLoc = newPosition.Y;
-		}
-
-		public void UpdatePosition(int newX, int newY)
-		{
-			_xLoc = newX;
-			_yLoc = newY;
-		}
-
-		public void MoveEntity(int deltaX, int deltaY)
-		{
-			_xLoc += deltaX;
-			_yLoc += deltaY;
-		}
-
-		public void AddTrait(EntityTraits trait) 
+		public void AddTrait(Traits trait) 
 		{
 			_traits.Add(trait);
 		}
 
-		public void RemoveTrait(EntityTraits trait) 
+		public void RemoveTrait(Traits trait) 
 		{
 			if (_traits.Contains(trait))
 				_traits.Remove(trait);
@@ -225,51 +112,17 @@ namespace Halfbreed.Entities
 				                                       "Entity: {0}, Trait: {1}", this, trait));
 		}
 
-		public bool HasTrait(EntityTraits trait)
+		public bool HasTrait(Traits trait)
 		{
 			return _traits.Contains(trait);
 		}
 
-		public int CompareTo(object obj)
-		{
-			return _displayLayer.CompareTo(((Entity)obj).DisplayLayer);
-		}
-
-		public bool HasComponent(ComponentType componentType)
-		{
-			return _components.ContainsKey(componentType);
-		}
-
-		public Component GetComponent(ComponentType componentType)
-		{
-			if (_components.ContainsKey(componentType))
-				return _components[componentType];
-			ErrorLogger.AddDebugText(string.Format("Asked for component type {0}, which is not present on {1}.", componentType, this));
-			return null;
-		}
-
-		public void Update(int currentTime)
+		public virtual void Update(Level currentLevel)
 		{
 			// TODO: Go through effects and check if any expire.
-
-			if (HasComponent(ComponentType.LIGHTSOURCE))
-			{
-				var lightSource = (LightSourceComponent)GetComponent(ComponentType.LIGHTSOURCE);
-				if (lightSource.IsLit)
-				{
-					lightSource.DecreaseDuration(1);
-					lightSource.UpdateLitTiles(GameEngine.CurrentLevel);
-				}
-			}
-
-			if (HasComponent(ComponentType.INPUT))
-			{
-				InputComponent inputComponent = (InputComponent)_components[ComponentType.INPUT];
-				inputComponent.GetNextMove(currentTime);
-			}
 		}
 
-		public Combat.Damage ProcessDamage(Entity attacker, Combat.Damage Damage)
+		public virtual Combat.Damage ProcessDamage(Entity attacker, Combat.Damage Damage)
 		{
 			MainGraphicDisplay.TextConsole.AddOutputText(string.Format("{0} takes {1} points of {2} damage",
 																	   this, Damage.FinalDamageAmount, Damage.DamageType));
