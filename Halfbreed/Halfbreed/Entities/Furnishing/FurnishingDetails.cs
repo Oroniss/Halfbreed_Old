6bbdece52a11fdb178be667965f747fda5683fc0
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-10 14:39:06 +1100

    Class to store details specific to Furnishings.

--- Halfbreed/Halfbreed/Entities/Furnishing/FurnishingDetails.cs
+++ Halfbreed/Halfbreed/Entities/Furnishing/FurnishingDetails.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 namespace Halfbreed
 {
-	public class FurnishingData
+	public class FurnishingDetails
 	{
-		public FurnishingData()
+		public readonly bool HasBGColor;
+		public readonly Colors BGColor;
+		public readonly bool HasFogColor;
+		public readonly Colors FogColor;
+		public readonly int Elevation;
+		public readonly string SetupFunction;
+
+		public FurnishingDetails(string bgColor, string fogColor, int elevation, string setupFunction)
 		{
+			if (bgColor == "")
+				HasBGColor = false;
+			else
+			{
+				HasBGColor = true;
+				BGColor = (Colors)Enum.Parse(typeof(Colors), bgColor);
+			}
+
+			if (fogColor == "")
+				HasFogColor = false;
+			else
+			{
+				HasFogColor = true;
+				FogColor = (Colors)Enum.Parse(typeof(Colors), fogColor);
+			}
+
+			Elevation = elevation;
+			SetupFunction = setupFunction;
 		}
 	}
 }
