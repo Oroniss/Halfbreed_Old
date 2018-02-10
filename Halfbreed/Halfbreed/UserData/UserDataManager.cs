35f371f69149fca10c45df6dc4621efb2c886ed7
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-10 14:35:26 +1100

    Added method shells. Got the configuration stuff working.

--- Halfbreed/Halfbreed/UserData/UserDataManager.cs
+++ Halfbreed/Halfbreed/UserData/UserDataManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Halfbreed.UserData;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
 
@@ -7,7 +8,10 @@ namespace Halfbreed
 	// TODO: Put this in application settings.
 	public static class UserDataManager
 	{
+		static bool _fullLogging;
+		
 		static string _configFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Config", "config.hb");
+		static readonly ConfigParameters _defaultConfigParameters = new ConfigParameters(false, false, false);
 
 		public static void SetupDirectoriesAndFiles()
 		{
@@ -24,7 +28,24 @@ namespace Halfbreed
 
 		static void CreateNewConfigFile()
 		{
+			WriteConfigFile(_defaultConfigParameters);
+		}
 
+		public static void WriteConfigFile(ConfigParameters configParameters)
+		{
+			var fileStream = File.OpenWrite(_configFilePath);
+			var serialiser = new BinaryFormatter();
+			serialiser.Serialize(fileStream, configParameters);
+			fileStream.Close();
+		}
+
+		public static ConfigParameters ReadConfigParameters()
+		{
+			var fileStream = File.OpenRead(_configFilePath);
+			var serialiser = new BinaryFormatter();
+			var configParameters = (ConfigParameters)serialiser.Deserialize(fileStream);
+			fileStream.Close();
+			return configParameters;
 		}
 
 		public static void WriteSaveGameSummary()
@@ -50,6 +71,11 @@ namespace Halfbreed
 		static void WriteAllGameSummaries()
 		{
 			
+		}
+
+		public static bool FullLogging
+		{
+			set { _fullLogging = value;}
 		}
 	}
 }
