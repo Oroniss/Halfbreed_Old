2ed86b777bdbeb0d9f19ba4f505136dbef004dc1
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-10 14:37:58 +1100

    Class for storing the basic details of an entity - used in Constructor.

--- Halfbreed/Halfbreed/Entities/EntityBasicDetails.cs
+++ Halfbreed/Halfbreed/Entities/EntityBasicDetails.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+
 namespace Halfbreed
 {
 	public class EntityBasicDetails
 	{
-		public EntityBasicDetails()
+		public readonly char Symbol;
+		public readonly Colors FGColor;
+		public readonly Traits[] Traits;
+
+		public EntityBasicDetails(char symbol, string colorName, string[] traits)
 		{
+			Symbol = symbol;
+			FGColor = (Colors)Enum.Parse(typeof(Colors), colorName);
+
+			var tmp = new Traits[traits.Length];
+			for (int i = 0; i < traits.Length; i++)
+				tmp[i] = (Traits)Enum.Parse(typeof(Traits), traits[i]);
+			Traits = tmp;
 		}
 	}
 }
