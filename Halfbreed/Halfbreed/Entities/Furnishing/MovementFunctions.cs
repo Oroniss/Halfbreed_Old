7dc58b63e46d59cf3a3307a118cf079e8b60d591
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-11 17:31:43 +1100

    New file to store furnishing based movement functions.

--- Halfbreed/Halfbreed/Entities/Furnishing/MovementFunctions.cs
+++ Halfbreed/Halfbreed/Entities/Furnishing/MovementFunctions.cs
@@ -1,10 +1,50 @@
-﻿using System;
-namespace Halfbreed
+﻿using System.Collections.Generic;
+
+namespace Halfbreed.Entities.Furnishings
 {
-	public class MovementFunctions
+	public static class MovementFunctions
 	{
-		public MovementFunctions()
+		static readonly Dictionary<string, MoveOffFunction> _moveOffFunctions = new Dictionary<string, MoveOffFunction>()
 		{
+		};
+
+		static readonly Dictionary<string, MoveOnFunction> _moveOnFunctions = new Dictionary<string, MoveOnFunction>()
+		{
+			{"Pit Trap Move On", new MoveOnFunction(PitTrapMoveOn)},
+			{"Flame Vent Trap Move On", new MoveOnFunction(FlameVentTrapMoveOn)}
+		};
+
+		public static MoveOffFunction GetMoveOffFunction(string functionName)
+		{
+			return _moveOffFunctions[functionName];
+		}
+
+		public static MoveOnFunction GetMoveOnFunction(string functionName)
+		{
+			return _moveOnFunctions[functionName];
+		}
+
+		public delegate bool MoveOffFunction(Furnishing furnishing, Actor actor, Level level, int destinationX, int destinationY);
+		public delegate void MoveOnFunction(Furnishing furnishing, Actor actor, Level level, int originX, int originY);
+
+		private static void PitTrapMoveOn(Furnishing furnishing, Actor actor, Level level, int originX, int originY)
+		{
+			if (actor.HasTrait(Traits.Flying))
+				return;
+
+			var trapLevel = int.Parse(furnishing.GetOtherAttributeValue("TrapLevel"));
+			actor.ProcessDamage(furnishing, new Combat.Damage(Combat.DamageType.PHYSICAL, 7 * trapLevel));
+		}
+
+		private static void FlameVentTrapMoveOn(Furnishing furnishing, Actor actor, Level level, int originX, int originY)
+		{
+			if (actor.HasTrait(Traits.Flying))
+				return;
+
+			var trapLevel = int.Parse(furnishing.GetOtherAttributeValue("TrapLevel"));
+			actor.ProcessDamage(furnishing, new Combat.Damage(Combat.DamageType.FIRE, 10 * trapLevel));
+			furnishing.Trapped = false;
+			furnishing.MoveOnFunction = null;
 		}
 	}
 }
