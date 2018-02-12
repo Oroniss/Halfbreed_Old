2e98f2473a5acb39402fd244348d19a3f0703a39
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-13 09:48:51 +1100

    Did a lot of cleaning up - code is nicer now. Probably still needs another pass or two.

--- Halfbreed/Halfbreed/Display/TextConsole.cs
+++ Halfbreed/Halfbreed/Display/TextConsole.cs
@@ -1,3 +1,5 @@
+// Updated for version 0.2.
+
 using RLNET;
 using System.Collections.Generic;
 
@@ -5,14 +7,14 @@ namespace Halfbreed.Display
 {
 	public class TextConsole:BaseConsole
 	{
-		private string[] _outputText;
-		private int _currentIndex;
+		string[] _outputText;
+		int _currentIndex;
 
-		private int _textXOffset = 1; // 1 Blank on the left
-		private int _textWidth;
-		private int _textYOffset = 3; // 1 Blank, 1 header, 1 more blank.
-		private int _textHeight;
-		private int _arrayLength = 100; // TODO: Put this into a config file somewhere.
+		readonly int _textXOffset = 1; // 1 Blank on the left
+		readonly int _textWidth;
+		readonly int _textYOffset = 3; // 1 Blank, 1 header, 1 more blank.
+		readonly int _textHeight;
+		const int _ARRAYLENGTH = 100; // TODO: Put this into a config file somewhere.
 
 		public TextConsole(int width, int height, int left, int top, RLColor backColor, BackConsole backConsole)
 			:base(width, height, left, top, backColor, backConsole)
@@ -21,42 +23,42 @@ namespace Halfbreed.Display
 			_textHeight = _console.Height - 4; // 3 at the top, 1 at the bottom.
 
 			_currentIndex = 0;
-			_outputText = new string[_arrayLength];
-			for (int i = 0; i < _outputText.Length; i++)
+			_outputText = new string[_ARRAYLENGTH];
+			for (var i = 0; i < _outputText.Length; i++)
 				_outputText[i] = "";
 		}
 
 		public void AddOutputText(string text)
 		{
 			// Don't want two blank lines in a row.
-			if (text == "" && CheckIfLastTextIsEmpty())
+			if (MessageIsEmpty(text) && LastMessageIsEmpty())
 				return;
 
 			if (text.Contains("\n"))
 			{
-				string[] splitText = text.Split('\n');
-				for (int i = splitText.Length - 1; i >= 0; i--)
+				var splitText = text.Split('\n');
+				for (var i = splitText.Length - 1; i >= 0; i--)
 					AddOutputText(splitText[i]);
 				return;
 			}
 
 			if (text.Length > _textWidth)
 			{
-				List<string> splitText = WrapText(text);
-				for (int i = splitText.Count - 1; i >= 0; i--)
+				var splitText = WrapText(text);
+				for (var i = splitText.Count - 1; i >= 0; i--)
 					AddOutputText(splitText[i]);
 				return;
 			}
 
 			_outputText[_currentIndex] = text;
-			_currentIndex = (_currentIndex + 1) % _arrayLength;
+			_currentIndex = (_currentIndex + 1) % _ARRAYLENGTH;
 
 			DrawOutputText();
 		}
 
-		private List<string> WrapText(string Text)
+		List<string> WrapText(string Text)
 		{
-			List<string> lines = new List<string>();
+			var lines = new List<string>();
 			while (true)
 			{
 				if (Text.Length <= _textWidth)
@@ -65,7 +67,7 @@ namespace Halfbreed.Display
 					return lines;
 				}
 
-				int lastSpace = Text.Substring(0, _textWidth).LastIndexOf(' ');
+				var lastSpace = Text.Substring(0, _textWidth).LastIndexOf(' ');
 				if (lastSpace == -1)
 				{
 					ErrorLogger.AddDebugText("Message text too long without a space character.");
@@ -76,20 +78,27 @@ namespace Halfbreed.Display
 			}
 		}
 
-		private bool CheckIfLastTextIsEmpty()
+		bool LastMessageIsEmpty()
 		{
-			return _outputText[(_currentIndex + (_arrayLength - 1)) % _arrayLength] == "";
+			return _outputText[(_currentIndex + (_ARRAYLENGTH - 1)) % _ARRAYLENGTH] == "";
+		}
+
+		bool MessageIsEmpty(string message)
+		{
+			return message == "";
 		}
 
 		public void DrawOutputText()
 		{
 			Clear();
 
-			_console.Print(_textXOffset + (int)((_textWidth - 11) / 2), 1, "Message Log", Palette.GetColor(Colors.White));
+			var messageLogTitle = string.Format("Message Log - Current Turn: {0}", GameEngine.CurrentTime);
+			_console.Print(_textXOffset + ((_textWidth - messageLogTitle.Length) / 2), 1, 
+			               messageLogTitle, Palette.GetColor(Colors.White));
 
 			for (int i = 0; i < _textHeight; i++)
 			{
-				int textIndex = (_currentIndex - 1 - i + _arrayLength) % _arrayLength;
+				int textIndex = (_currentIndex - 1 - i + _ARRAYLENGTH) % _ARRAYLENGTH;
 				_console.Print(_textXOffset, _textYOffset + i, _outputText[textIndex], Palette.GetColor(Colors.White));
 			}
 
