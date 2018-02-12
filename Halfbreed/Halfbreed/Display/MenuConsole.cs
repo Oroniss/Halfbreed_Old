db15ae5524508c0dd2a4a34ce4bcda19765e8645
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-13 09:48:22 +1100

    Made some minor changes and tidying - still needs a bit more thinking through.

--- Halfbreed/Halfbreed/Display/MenuConsole.cs
+++ Halfbreed/Halfbreed/Display/MenuConsole.cs
@@ -1,3 +1,5 @@
+// Tidied up for version 0.2.
+
 using RLNET;
 using System.Collections.Generic;
 
@@ -18,20 +20,24 @@ namespace Halfbreed.Display
 			_console.Print(5, 5, title, Palette.GetColor(Colors.Black));
 
 			// TODO: Figure out whether we can dynamically space the menu between 1 and 4 spaces.
-			for (int i = 0; i < options.Count; i++)
+			// TODO: Split each piece out and put them in a new array - then use the length of the new array to sort it out.
+			for (var i = 0; i < options.Count; i++)
 			{
 				if (options[i].Contains("\n"))
 				{
 					var pieces = options[i].Split('\n');
-					// TODO: Add a check for more than 3 pieces.
-					for (int j = 0; j < pieces.Length; j++)
-						_console.Print(5, 10 + 4 * i + j, pieces[j], Palette.GetColor(Colors.Black));
+
+					if (pieces.Length > 4)
+						ErrorLogger.AddDebugText("Two many lines in menu item" + pieces.ToString());
+
+					for (var j = 0; j < pieces.Length; j++)
+						_console.Print(5, 10 + 5 * i + j, pieces[j], Palette.GetColor(Colors.Black));
 				}
 				else
-					_console.Print(5, 10 + 4 * i, options[i], Palette.GetColor(Colors.Black));
+					_console.Print(5, 10 + 5 * i, options[i], Palette.GetColor(Colors.Black));
 			}
 
-			_console.Print(5, 60, bottom, Palette.GetColor(Colors.Black));
+			_console.Print(5, 70, bottom, Palette.GetColor(Colors.Black));
 
 			CopyToBackConsole();
 		}
