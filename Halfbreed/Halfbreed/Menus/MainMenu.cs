5f08f6288a70853acc056588bde233935b5103f1
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-17 12:50:08 +1100

    Significantly reworked - the responsibility for starting and running the game was moved out of here.

--- Halfbreed/Halfbreed/Menus/MainMenu.cs
+++ Halfbreed/Halfbreed/Menus/MainMenu.cs
@@ -1,52 +1,44 @@
 using System.Collections.Generic;
-using System.IO;
 
 namespace Halfbreed.Menus
 {
 	public class MainMenu
 	{
-		private static List<string> _mainMenuOptions = new List<string>{"New Game", "Load Game", "View Achievements",
+		static List<string> _mainMenuOptions = new List<string>{"New Game", "Load Game", "View Achievements",
 			"Clear Achievements", "View Commands", "Config"};
 
-		public void TitleMenu()
+		public int DisplayMainMenu()
 		{
 			while (true)
 			{
-				int selection = UserInputHandler.SelectFromMenu("Welcome to Halfbreed", _mainMenuOptions, "Escape to Quit");
+				var selection = UserInputHandler.SelectFromMenu("Welcome to Halfbreed", _mainMenuOptions, "Escape to Quit");
 
 				switch (selection)
 				{
 					case 0:
 						{
-							NewGameParameters parameters = MenuProvider.CharacterCreationMenu.StartNewGame();
-							if (!parameters.Cancel)
-							{
-								parameters.GameID = 0;  //UserDatabaseConnection.GenerateNextGameId();
-								GameEngine.SetupNewGame(parameters);
-								Levels.LevelEnum startingLevel = Levels.LevelEnum.TESTLEVEL1;
-								GameEngine.LevelTransition(startingLevel, 42, 5);
-								//Levels.LevelEnum startingLevel = Levels.LevelEnum.TESTLEVEL2;
-								//GameEngine.LevelTransition(startingLevel, 49, 42);
-								//UserDatabaseConnection.InsertNewSaveGameSummary(GameEngine.GenerateSaveSummary());
-								GameEngine.RunGame();
-							}
-							MainProgram.quit();
-							return;
+							GameData parameters = MenuProvider.CharacterCreationMenu.StartNewGame();
+							if (parameters == null)
+								return -1;
+
+							// Create new save game summary.
+							// Return game id.
+							return 0;
 						}
 					case 1:
 						{
 							//int gameId = MenuProvider.LoadGameMenu.SelectSavedGame();
+							// return gameId;
 							break;
-						}
-					case -1:
-						{
-							MainProgram.quit();
-							return;
 						}
 					case 5:
 						{
 							UserInputHandler.DisplayConfigMenu();
 							break;
+						}
+					case -1:
+						{
+							return -1;
 						}
 				}
 			}
