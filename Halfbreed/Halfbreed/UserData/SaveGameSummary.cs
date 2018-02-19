fb5e6568bc4fb8f2836fab647011f1fa19f57c34
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-20 09:49:48 +1100

    Pulled a lot off onto GameData - made a few other changes as well.

--- Halfbreed/Halfbreed/UserData/SaveGameSummary.cs
+++ Halfbreed/Halfbreed/UserData/SaveGameSummary.cs
@@ -2,39 +2,26 @@ using System;
 
 namespace Halfbreed.UserData
 {
+	[Serializable]
 	public struct SaveGameSummary
 	{
-		public int GameId;
-		public int DifficultySetting;
-		public CharacterClasses CharacterClass;
-		public bool UseAchievements;
-		public int CurrentAct;
-		public int CurrentChapter;
+		public GameData GameData;
+		public string CurrentLevelName;
 		public bool StillAlive;
 		public long LastSaveTime;
 
-		public SaveGameSummary(int gameId, int difficultySetting, CharacterClasses characterClass,
-							   bool useAchievements, int currentAct, int currentChapter, bool stillAlive, DateTime lastSaveTime)
+		public SaveGameSummary(GameData gameData, string currentLevelName, bool stillAlive, DateTime lastSaveTime)
 		{
-			GameId = gameId;
-			DifficultySetting = difficultySetting;
-			CharacterClass = characterClass;
-			UseAchievements = useAchievements;
-			CurrentAct = currentAct;
-			CurrentChapter = currentChapter;
+			GameData = gameData;
 			StillAlive = stillAlive;
+			CurrentLevelName = currentLevelName;
 			LastSaveTime = ((DateTimeOffset)lastSaveTime).ToUnixTimeSeconds();
 		}
 
-		public SaveGameSummary(int gameId, int difficultySetting, CharacterClasses characterClass,
-					   bool useAchievements, int currentAct, int currentChapter, bool stillAlive, long lastSaveTime)
+		public SaveGameSummary(GameData gameData, string currentLevelName, bool stillAlive, long lastSaveTime)
 		{
-			GameId = gameId;
-			DifficultySetting = difficultySetting;
-			CharacterClass = characterClass;
-			UseAchievements = useAchievements;
-			CurrentAct = currentAct;
-			CurrentChapter = currentChapter;
+			GameData = gameData;
+			CurrentLevelName = currentLevelName;
 			StillAlive = stillAlive;
 			LastSaveTime = lastSaveTime;
 		}
@@ -42,23 +29,24 @@ namespace Halfbreed.UserData
 		public override string ToString()
 		{
 			DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-			dtDateTime = dtDateTime.AddSeconds( LastSaveTime ).ToLocalTime();
+			dtDateTime = dtDateTime.AddSeconds(LastSaveTime).ToLocalTime();
 
 			string achString = "Off";
-			if (UseAchievements)
+			if (GameData.UseAchievements)
 				achString = "On";
-			
+
 			string[] items = new string[] {
-				CharacterClass.ToString(), 
-				DifficultySetting.ToString(),
-				achString, 
-				CurrentAct.ToString(),
-				CurrentChapter.ToString(),
+				GameData.CharacterClass.ToString(),
+				GameData.DifficultySetting.ToString(),
+				achString,
+				CurrentLevelName,
 				dtDateTime.ToString(),
+				GameData.CharacterNote
 				};
-			
+
 			string returnString = "Character Class: {0}, Difficulty: {1}, Use Previous Achievements: {2}" +
-				"\nCurrent Act: {3}, Current Chapter: {4}, Last Save Time: {5}.";
+				"\n, Current Level: {3}, Last Save Time: {4}." +
+				"\n, {5}";
 			
 			return string.Format(returnString, items);
 		}
