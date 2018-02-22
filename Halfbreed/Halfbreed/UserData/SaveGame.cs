3681917a03837b9ab9dd194f30d3edaccb50f4dc
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-22 22:43:03 +1100

    Packed up the pieces that need to go on a save game.

--- Halfbreed/Halfbreed/UserData/SaveGame.cs
+++ Halfbreed/Halfbreed/UserData/SaveGame.cs
@@ -1,7 +1,24 @@
 ﻿using System;
-namespace Halfbreed
+
+namespace Halfbreed.UserData
 {
-	public struct SaveGame
+	[Serializable]
+	public class SaveGame
 	{
+		public SaveGameSummary Summary;
+		public Level CurrentLevel;
+		public Entities.Player Player;
+		public int CurrentTime;
+		public Levels.TileType[] MapGrid;
+
+		public SaveGame(SaveGameSummary summary, Level currentLevel, Entities.Player player, int currentTime,
+		               Levels.TileType[] mapGrid)
+		{
+			Summary = summary;
+			CurrentLevel = currentLevel;
+			Player = player;
+			CurrentTime = currentTime;
+			MapGrid = mapGrid;
+		}
 	}
 }
