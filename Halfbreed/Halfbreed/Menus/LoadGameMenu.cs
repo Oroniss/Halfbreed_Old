babbce6d8b983593e757b42a32df606d4611b1a1
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-22 22:42:08 +1100

    Added a delete save game option as well.

--- Halfbreed/Halfbreed/Menus/LoadGameMenu.cs
+++ Halfbreed/Halfbreed/Menus/LoadGameMenu.cs
@@ -8,12 +8,21 @@ namespace Halfbreed.Menus
 		
 		public int SelectSavedGame()
 		{
+			return DisplaySaveMenu("Select your champion");
+		}
+
+		public int DeleteSavedGame()
+		{
+			return DisplaySaveMenu("Which champion do you wish to remove? This is irrevocable!");
+		}
+
+		int DisplaySaveMenu(string menuText)
+		{
 			List<SaveGameSummary> currentSaves = UserDataManager.GetCurrentSaves();
 			List<string> saveStrings = new List<string>();
 			foreach (SaveGameSummary saveSummary in currentSaves)
 				saveStrings.Add(saveSummary.ToString());
 			
-			string menuText = "Select your champion";
 			if (currentSaves.Count == 0)
 				menuText = "Sorry, there are no surviving champions";
 
@@ -23,9 +32,8 @@ namespace Halfbreed.Menus
 				return -1;
 
 			return currentSaves[selection].GameData.GameID;
+			
 		}
-
-	
 	
 	}
 }
