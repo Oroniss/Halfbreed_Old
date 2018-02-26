c3f9031451b18c24810312c0566fcf2a79419390
jeffrey <jeffrey.fisher@anu.edu.au>
2018-02-27 00:13:19 +1100

    Pulled out of the MainProgram file into a class of its own.

---
+++ Halfbreed/Halfbreed/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Halfbreed
+{
+	[Serializable]
+	public class GameData
+	{
+		public int DifficultySetting;
+		public CharacterClasses CharacterClass;
+		public bool UseAchievements;
+		public string CharacterNote;
+		public int GameID;
+
+		public GameData()
+		{
+			DifficultySetting = 1;
+			CharacterClass = CharacterClasses.Fighter;
+			UseAchievements = true;
+			CharacterNote = "";
+		}
+
+		public GameData(int difficulty, CharacterClasses characterClass, bool useAchievements, string characterNote,
+						int gameId)
+		{
+			DifficultySetting = difficulty;
+			CharacterClass = characterClass;
+			UseAchievements = useAchievements;
+			CharacterNote = characterNote;
+			GameID = gameId;
+		}
+	}
+}
