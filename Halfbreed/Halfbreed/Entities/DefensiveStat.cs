1f8f27b054e25ac7597d10862e8e97cf80004834
jeffrey <jeffrey.fisher@anu.edu.au>
2018-03-12 00:34:19 +1100

    A defensive stat - needs to be a bit different.

--- Halfbreed/Halfbreed/Entities/DefensiveStat.cs
+++ Halfbreed/Halfbreed/Entities/DefensiveStat.cs
@@ -1,10 +1,51 @@
 ﻿using System;
-namespace Halfbreed
+using System.Collections.Generic;
+
+namespace Halfbreed.Entities
 {
+	[Serializable]
 	public class DefensiveStat
 	{
+		List<Dice> _dice;
+
 		public DefensiveStat()
 		{
+			_dice = new List<Dice>();
+			for (int i = 0; i < 5; i++)
+				_dice.Add(new Dice(DiceType.D3, 1));
+		}
+
+		void OrderDice()
+		{
+			_dice.Sort();
+			_dice.Reverse();
+		}
+
+		public void AddDefensiveDice(DiceType diceType, int upgradeLevel)
+		{
+			_dice.Add(new Dice(diceType, upgradeLevel));
+			OrderDice();
+		}
+
+		public void RemoveDefensiveDice(DiceType diceType, int upgradeLevel)
+		{
+			for (int i = 0; i < _dice.Count; i++)
+			{
+				if (_dice[i].DiceType == diceType && _dice[i].UpgradeLevel == upgradeLevel)
+				{
+					_dice.RemoveAt(i); // No need to sort here.
+					return;
+				}
+			}
+			// TODO: Print an error message here.
+		}
+
+		public Dice[] GetDefensiveDice()
+		{
+			var returnArray = new Dice[5];
+			for (int i = 0; i < 5; i++)
+				returnArray[i] = new Dice(_dice[i].DiceType, _dice[i].UpgradeLevel);
+			return returnArray;
 		}
 	}
 }
